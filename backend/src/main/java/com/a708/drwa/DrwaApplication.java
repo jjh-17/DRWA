b558ad5eabfb8f4ee54429d3b0b61713d42cc5e3
lkt9899 <lkt9899@naver.com>
2024-01-18 11:26:00 +0900

    test init

---
+++ backend/src/main/java/com/a708/drwa/DrwaApplication.java
@@ -0,0 +1,13 @@
+package com.a708.drwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrwaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/a708/drwa/DrwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a708.drwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
