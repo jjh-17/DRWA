5b6e68e0e018765b768ac50b62f7329c044a4512
jjho311 <jjho311@naver.com>
2024-01-21 22:25:28 +0900

    Feat : 전적 저장 요청 Dto 추가

---
+++ backend/src/main/java/com/a708/drwa/record/domain/data/dto/request/RecordRequestDto.java
@@ -0,0 +1,27 @@
+package com.a708.drwa.record.domain.data.dto.request;
+
+import com.a708.drwa.record.domain.Result;
+import com.a708.drwa.record.domain.Team;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class RecordRequestDto {
+    @NotNull
+    private int memberId;
+
+    @NotNull
+    private int gameId;
+
+    @Enumerated(EnumType.ORDINAL)
+    @NotNull
+    private Result result;
+
+    @Enumerated(EnumType.ORDINAL)
+    @NotNull
+    private Team team;
+}
