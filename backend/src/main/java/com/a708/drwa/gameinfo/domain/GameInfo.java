12b5b9307a2dfadbb4f500ea8be84f697bdb9b10
jjho311 <jjho311@naver.com>
2024-01-21 21:59:57 +0900

    Feat : 게임 정보 도메인 추가

---
+++ backend/src/main/java/com/a708/drwa/gameinfo/domain/GameInfo.java
@@ -0,0 +1,35 @@
+package com.a708.drwa.gameinfo.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GameInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int gameId;
+
+    @Column(name="keyword", nullable = false)
+    @Size(max = 31)
+    private String keyword;
+
+    @Column(name = "mvp_member_id", nullable = false)
+    private int mvpMemberId;
+
+    @Builder
+    public GameInfo(
+            String keyword,
+            int mvpMemberId
+    ) {
+        this.keyword = keyword;
+        this.mvpMemberId = mvpMemberId;
+    }
+
+}
