6568f4e0c52abe60fb492459983f5e27f9bdcbf4
jjho311 <jjho311@naver.com>
2024-01-21 22:30:22 +0900

    Feat : 전적 추가 서비스 추가

---
+++ backend/src/main/java/com/a708/drwa/record/service/RecordService.java
@@ -0,0 +1,27 @@
+package com.a708.drwa.record.service;
+
+import com.a708.drwa.record.domain.Record;
+import com.a708.drwa.record.domain.data.dto.request.RecordRequestDto;
+import com.a708.drwa.record.repository.RecordRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class RecordService {
+
+    private final RecordRepository recordRepository;
+
+    @Transactional
+    public Record createRecord(RecordRequestDto recordRequestDto) {
+        Record record = Record.builder()
+                .memberId(recordRequestDto.getMemberId())
+                .gameId(recordRequestDto.getGameId())
+                .result(recordRequestDto.getResult())
+                .team(recordRequestDto.getTeam())
+                .build();
+
+        return recordRepository.save(record);
+    }
+}
