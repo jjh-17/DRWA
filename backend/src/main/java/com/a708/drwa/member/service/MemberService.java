1900198aa05de673abdc7d82ad896779c7c7181c
minwoo-kwak <minwoo.kwak540@gmail.com>
2024-01-22 16:53:20 +0900

    Feat : 소셜로그인 및 회원등록 구현
    구글, 네이버, 카카오 모두 필요한 기능이라 MemberService에서 구현하여
    중복 코드를 최소화하고자 하였습니다.
    
    소셜로그인 과정에서 회원정보가 없을 경우 회원가입이 진행됩니다

--- backend/src/main/java/com/a708/drwa/member/service/MemberService.java
+++ backend/src/main/java/com/a708/drwa/member/service/MemberService.java
@@ -1,6 +1,9 @@
 package com.a708.drwa.member.service;
 
+import com.a708.drwa.member.domain.Member;
+import com.a708.drwa.member.dto.SocialUserInfoResponse;
 import com.a708.drwa.member.repository.MemberRepository;
+import com.a708.drwa.member.type.SocialType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,4 +14,29 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
     private final MemberRepository memberRepository;
 
+    /**
+     * 소셜 로그인 처리
+     * @param userInfo 사용자 정보
+     * @return 처리된 사용자 정보
+     */
+    @Transactional
+    public Member handleSocialLogin(SocialUserInfoResponse userInfo) {
+        // 사용자 정보를 기반으로 기존 사용자 조회
+        Member member = memberRepository.findByUserId(userInfo.getId())
+                // 기존 사용자가 없으면 새로운 사용자 등록
+                .orElseGet(() -> registerNewUser(userInfo));
+
+        return member;
+    }
+
+    /**
+     * 새로운 사용자 등록
+     * @param userInfo 사용자 정보
+     * @return 등록된 사용자 정보
+     */
+    private Member registerNewUser(SocialUserInfoResponse userInfo) {
+        Member newMember = Member.createMember(userInfo.getId(), userInfo.getSocialType());
+
+        return memberRepository.save(newMember);
+    }
 }
