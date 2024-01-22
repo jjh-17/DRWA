a02588740034bf93664cea3d7a6e9ffe90af9f5b
minwoo-kwak <minwoo.kwak540@gmail.com>
2024-01-22 16:34:58 +0900

    Feat : 소셜로그인 인증 및 토큰요청 REST API 컨트롤러 구현
    인증요청 시 인증 페이지를 반환하고
    인증 성공으로 받은 코드로 토큰을 요청하고
    요청받은 토큰으로 회원정보를 검색하고 가져옵니다.
    회원정보가 없을 시 자동 회원가입을 합니다.

--- backend/src/main/java/com/a708/drwa/member/controller/MemberController.java
+++ backend/src/main/java/com/a708/drwa/member/controller/MemberController.java
@@ -1,11 +1,103 @@
 package com.a708.drwa.member.controller;
 
+import com.a708.drwa.member.domain.Member;
+import com.a708.drwa.member.dto.SocialAuthURLResponse;
+import com.a708.drwa.member.dto.SocialUserInfoResponse;
+import com.a708.drwa.member.service.Impl.GoogleLoginServiceImpl;
+import com.a708.drwa.member.service.Impl.KakaoLoginServiceImpl;
+import com.a708.drwa.member.service.Impl.NaverLoginServiceImpl;
+import com.a708.drwa.member.service.MemberService;
+import com.a708.drwa.member.service.SocialLoginService;
+import com.a708.drwa.member.type.SocialType;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+
+    private final GoogleLoginServiceImpl googleLoginService;
+    private final NaverLoginServiceImpl naverLoginService;
+    private final KakaoLoginServiceImpl kakaoLoginService;
+    private final MemberService memberService;
+
+    /**
+     * 소셜 로그인 인증 URL을 반환한다.
+     * @param socialType : google, naver, kakao
+     * @return : 인증 URL
+     */
+    @GetMapping("/authURL/{socialType}")
+    public ResponseEntity<SocialAuthURLResponse> getAuthURL(@PathVariable String socialType) {
+        // 소셜 로그인 서비스
+        SocialLoginService service = getSocialLoginService(socialType);
+        // 응답 DTO
+        SocialAuthURLResponse resposneDto = new SocialAuthURLResponse();
+
+        // 지원하지 않는 소셜 로그인 타입
+        if (service == null) {
+            // 응답 DTO에 에러 메시지 저장
+            resposneDto.setErrorMessage("Unsupported social login type");
+            // 400 Bad Request 반환
+            return ResponseEntity.badRequest().body(resposneDto);
+        }
+
+        // 인증 URL 생성
+        String authorizationUrl = service.getAuthorizationUrl();
+        resposneDto.setAuthorizationUrl(authorizationUrl);
+        return ResponseEntity.ok(resposneDto);
+    }
+
+    /**
+     * 소셜 로그인 후 인증 코드로부터 액세스 토큰을 반환한다.
+     * @param socialType : google, naver, kakao
+     * @param code : 소셜 로그인 후 인증 코드
+     * @return : 액세스 토큰
+     */
+    @GetMapping("/login/{socialType}")
+    public ResponseEntity<?> socialLogin(@PathVariable String socialType, @RequestParam String code) {
+        SocialLoginService service = getSocialLoginService(socialType);
+
+        // 지원하지 않는 소셜 로그인 타입
+        if (service == null) {
+            return ResponseEntity.badRequest().body("Unsupported social login type");
+        }
+
+        // 액세스 토큰 반환
+        String accessToken = service.getAccessToken(code);
+
+        // 액세스 토큰으로부터 사용자 정보를 반환한다.
+        SocialUserInfoResponse responseUserInfoDto = service.getUserInfo(accessToken);
+
+        // 소셜로그인 타입 설정
+        responseUserInfoDto.setSocialType(SocialType.fromString(socialType));
+
+        Member member = memberService.handleSocialLogin(responseUserInfoDto);
+
+        return ResponseEntity.ok(accessToken);
+    }
+
+
+    /**
+     * 소셜 로그인 서비스를 반환한다.
+     * @param socialType : google, naver, kakao
+     * @return : 소셜 로그인 서비스
+     */
+    private SocialLoginService getSocialLoginService(String socialType) {
+        switch (socialType) {
+            case "google":
+                return googleLoginService;
+            case "naver":
+                return naverLoginService;
+            case "kakao":
+                return kakaoLoginService;
+            default: // 지원하지 않는 소셜 로그인 타입
+                return null;
+        }
+    }
+
+
+
+
 }
