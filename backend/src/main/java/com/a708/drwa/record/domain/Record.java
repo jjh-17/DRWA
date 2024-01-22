6d191974e869c22e3ac4644c11fa9415c2556577
jjh <jjho311@naver.com>
2024-01-22 13:08:34 +0900

    Fix : 레코드 추가 실패 해결 - @ManyToOne 용 변수와 실제 저장될 id 값을 동시에 선언 - DB 내에는 id만 저장됨

--- backend/src/main/java/com/a708/drwa/record/domain/Record.java
+++ backend/src/main/java/com/a708/drwa/record/domain/Record.java
@@ -3,10 +3,12 @@ package com.a708.drwa.record.domain;
 import com.a708.drwa.gameinfo.domain.GameInfo;
 import com.a708.drwa.member.domain.Member;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 @Entity
 @Getter
@@ -17,12 +19,19 @@ public class Record {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int recordId;
 
-    @ManyToOne(targetEntity = Member.class)
-    @JoinColumn(name="member_id", nullable = false)
+    @ManyToOne(targetEntity = Member.class, fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id", insertable = false, updatable = false)
+    private Member member;
+
+    @Column(name="member_id", nullable = false)
+    @Range(min=0, max=3)
     private int memberId;
 
-    @ManyToOne(targetEntity = GameInfo.class)
-    @JoinColumn(name="game_id", nullable = false)
+    @ManyToOne(targetEntity = GameInfo.class, fetch = FetchType.LAZY)
+    @JoinColumn(name="game_id", insertable = false, updatable = false)
+    private GameInfo gameInfo;
+
+    @Column(name="game_id", nullable = false)
     private int gameId;
 
     @Enumerated(EnumType.ORDINAL)
