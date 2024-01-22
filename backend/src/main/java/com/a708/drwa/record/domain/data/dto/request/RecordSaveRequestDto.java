d7a827eca726e33dfd7cfca8603f60774c2bdff4
jjh <jjho311@naver.com>
2024-01-22 12:58:57 +0900

    CHORE : 설정 파일 내 queryDSL implementation 겹침 주석

--- backend/src/main/java/com/a708/drwa/record/domain/data/dto/request/RecordSaveRequestDto.java
+++ backend/src/main/java/com/a708/drwa/record/domain/data/dto/request/RecordSaveRequestDto.java
@@ -5,12 +5,14 @@ import com.a708.drwa.record.domain.Team;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
-public class RecordRequestDto {
+public class RecordRequestSaveDto {
+
     @NotNull
     private int memberId;
 
@@ -24,4 +26,15 @@ public class RecordRequestDto {
     @Enumerated(EnumType.ORDINAL)
     @NotNull
     private Team team;
+
+    @Builder
+    public RecordRequestSaveDto(int memberId, int gameId, Result result, Team team){
+        this.memberId = memberId;
+        this.gameId = gameId;
+        this.result = result;
+        this.team = team;
+    }
+
+    public 
+
 }
