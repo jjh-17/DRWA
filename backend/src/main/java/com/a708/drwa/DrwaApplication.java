2e8419159a2a734a34cc096dc16d891f39d1c4f5
lkt9899 <lkt9899@naver.com>
2024-01-22 11:10:43 +0900

    Feat : 시큐리티 설정 임시 제외

--- backend/src/main/java/com/a708/drwa/DrwaApplication.java
+++ backend/src/main/java/com/a708/drwa/DrwaApplication.java
@@ -2,8 +2,11 @@ package com.a708.drwa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableCaching
 public class DrwaApplication {
 
 	public static void main(String[] args) {
