96adb5d3d3053d342b057620e68aac6a2c4ef21f
KimJihwan98 <wlghks98k@gmail.com>
2024-01-22 17:54:03 +0900

    Feat : 세션 에러 종류 추가

--- backend/src/main/java/com/a708/drwa/debate/exception/DebateErrorCode.java
+++ backend/src/main/java/com/a708/drwa/debate/exception/DebateErrorCode.java
@@ -1,8 +1,12 @@
 package com.a708.drwa.debate.exception;
 
-public enum DebateErrorCode {
-    NOT_EXIST_DEBATE_ROOM_ERROR(404, "DEBATE_01", "존재하지 않는 토론 방입니다.");
+import com.a708.drwa.global.exception.ErrorCode;
+import lombok.Getter;
 
+@Getter
+public enum DebateErrorCode implements ErrorCode {
+    NOT_EXIST_DEBATE_ROOM_ERROR(404, "DEBATE_01", "존재하지 않는 토론 방입니다."),
+    ALREADY_EXIST_DEBATE_ROOM_ERROR(400, "DEBATE_02", "이미 존재하는 토론 방입니다.");
     private final int statusCode;
     private final String errorCode;
     private final String message;
