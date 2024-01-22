d0a48e2bb2d95e344cb098a02663d9e184597173
minwoo-kwak <minwoo.kwak540@gmail.com>
2024-01-22 16:40:14 +0900

    Feat : Member Entity 컬럼 추가 및 빌더 패턴 추가
    소셜로그인 시 가져오는 사용자 ID(String) 과 소셜로그인 타입 정보 컬럼을
    생상하였습니다. 그리고 생성자가 Protected로 되어 있어서 MemberService의
    등록 메서드에서 사용하기 위해 Builder 패턴을 적용하였습니다

--- backend/src/main/java/com/a708/drwa/member/domain/Member.java
+++ backend/src/main/java/com/a708/drwa/member/domain/Member.java
@@ -1,19 +1,40 @@
 package com.a708.drwa.member.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.a708.drwa.member.type.SocialType;
+import jakarta.persistence.*;
+import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Getter
 @Entity
+@Builder
 public class Member {
 
+    // Primary Key
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int memberId;
+
+    // 소셜 로그인 사용자 ID
+    @Column(unique = true)
+    private String userId;
+
+    @Column
+    private SocialType socialType;
+
+    public static Member createMember(String userId, SocialType socialType) {
+        return Member.builder()
+                .userId(userId)
+                .socialType(socialType)
+                .build();
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setSocialType(SocialType socialType) {
+        this.socialType = socialType;
+    }
 }
