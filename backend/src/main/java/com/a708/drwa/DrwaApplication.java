4a356bcb0e65feda79f3084d4af873d9bf0b9984
jjh <jjho311@naver.com>
2024-01-22 13:03:17 +0900

    Chore : SpringSecurity에 의한 로그인 페이지 무시

--- backend/src/main/java/com/a708/drwa/DrwaApplication.java
+++ backend/src/main/java/com/a708/drwa/DrwaApplication.java
@@ -2,8 +2,9 @@ package com.a708.drwa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class DrwaApplication {
 
 	public static void main(String[] args) {
