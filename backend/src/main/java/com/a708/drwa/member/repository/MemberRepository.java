98eeb73e50bb2bf4c9fcc188904f82d5a8bd2b52
minwoo-kwak <minwoo.kwak540@gmail.com>
2024-01-22 16:47:01 +0900

    Feat : findByUserId(String) 추가

--- backend/src/main/java/com/a708/drwa/member/repository/MemberRepository.java
+++ backend/src/main/java/com/a708/drwa/member/repository/MemberRepository.java
@@ -3,5 +3,10 @@ package com.a708.drwa.member.repository;
 import com.a708.drwa.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Integer> {
+    Optional<Member> findByMemberId(int memberId);
+
+    Optional<Member> findByUserId(String userId);
 }
