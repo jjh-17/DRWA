517d110fb844fb4ff31714f80d2cbcc353ffb221
lkt9899 <lkt9899@naver.com>
2024-01-23 17:45:01 +0900

    Style : DI constructor 방식으로 수정 - @Autowired를 통한 property 주입은 권장되지 않습니다. @RequiredArgsConstructor를 이용해 주세요.

--- backend/src/main/java/com/a708/drwa/room/service/RoomService.java
+++ backend/src/main/java/com/a708/drwa/room/service/RoomService.java
@@ -2,17 +2,15 @@ package com.a708.drwa.room.service;
 
 import com.a708.drwa.room.domain.Room;
 import com.a708.drwa.room.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class RoomService {
-
-    @Autowired
-    private RoomRepository roomRepository;
-
-
+    private final RoomRepository roomRepository;
 
     public Room getRoomByTitle(String title){//제목 검색해서 검색어에 해당하는 방 가정오기
         Optional<Room> roomOptional = roomRepository.findByTitle(title);
