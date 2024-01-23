e75d0dff9318573086b75483226ed844d0c5bde7
lkt9899 <lkt9899@naver.com>
2024-01-23 17:43:15 +0900

    Chore : 설정 파일 수정

--- backend/src/main/java/com/a708/drwa/DrwaApplication.java
+++ backend/src/main/java/com/a708/drwa/DrwaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableCaching
