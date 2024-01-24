10965d01d7626e63f5dc8d71d8e95abdafb5605d
jjh <jjho311@naver.com>
2024-01-24 17:42:52 +0900

    Feat : mvpMemberId 컬럼 수정 메서드, Redis와의 통신 메서드 틀 추가

--- backend/src/main/java/com/a708/drwa/game/repository/GameInfoRepository.java
+++ backend/src/main/java/com/a708/drwa/game/repository/GameInfoRepository.java
@@ -1,7 +1,27 @@
 package com.a708.drwa.game.repository;
 
+import com.a708.drwa.game.data.dto.request.GameInfoCreateRequestRedisDto;
+import com.a708.drwa.game.data.dto.response.GameInfoCreateResponseRedisDto;
 import com.a708.drwa.game.domain.GameInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GameInfoRepository extends JpaRepository<GameInfo, Integer> {
+
+    // MVP 선정 멤버 ID 수정
+    @Query("UPDATE GameInfo g SET g.mvpMemberId=:memberId WHERE g.gameId=:gameId")
+    int updateMvp(int gameId, int memberId);
+
+    // Redis에서 게임 정보 생성을 위해 필요한 데이터를 받아옴
+    default GameInfoCreateResponseRedisDto getGameInfoCreateResponseRedisDto(GameInfoCreateRequestRedisDto gameInfoCreateRequestRedisDto) {
+        // TODO : Redis에서 데이터 가져올 것!
+        String keyword = "keyword";
+
+        return GameInfoCreateResponseRedisDto.builder()
+                .keyword(keyword)
+                .build();
+    }
+
 }
