aa2ed6d8e342b0ac3dc0cdde689a33dd5d370511
KimJihwan98 <wlghks98k@gmail.com>
2024-01-24 17:43:12 +0900

    Feat : 토론 방 생성부터 종료까지 메세지 추가

---
+++ backend/src/main/java/com/a708/drwa/redis/message/DebateSettingMessage.java
@@ -0,0 +1,68 @@
+package com.a708.drwa.redis.message;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Setter;
+
+@Setter
+@JsonSerialize
+public class DebateSettingMessage {
+    // 페이즈 변경 알려줌
+    private String event;
+
+    // 방 생성
+    private String title;
+    private String keyword;
+    private Boolean isPrivate;
+    private Integer password;
+    // playerNum 2->1:1      4->2:2     6->3:3
+    private Integer playerNum;
+    private Integer opinionTime;
+    private Integer readyTime;
+    private Integer questionTime;
+
+    @JsonProperty
+    public String getEvent() {
+        return event;
+    }
+
+    @JsonProperty
+    public String getTitle() {
+        return title;
+    }
+
+    @JsonProperty
+    public String getKeyword() {
+        return keyword;
+    }
+
+    @JsonProperty
+    public Boolean getPrivate() {
+        return isPrivate;
+    }
+
+    @JsonProperty
+    public Integer getPassword() {
+        return password;
+    }
+
+    @JsonProperty
+    public Integer getPlayerNum() {
+        return playerNum;
+    }
+
+    @JsonProperty
+    public Integer getOpinionTime() {
+        return opinionTime;
+    }
+
+    @JsonProperty
+    public Integer getReadyTime() {
+        return readyTime;
+    }
+
+    @JsonProperty
+    public Integer getQuestionTime() {
+        return questionTime;
+    }
+}
