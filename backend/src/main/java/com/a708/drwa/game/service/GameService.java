7b21736e52702c4b3c3e49fd577cfdbc6f0b63ca
jjh <jjho311@naver.com>
2024-01-24 17:44:49 +0900

    Chore : Redis 및 MySql과의 통신 서비스 개발 진행 중

--- backend/src/main/java/com/a708/drwa/game/service/GameService.java
+++ backend/src/main/java/com/a708/drwa/game/service/GameService.java
@@ -1,9 +1,17 @@
 package com.a708.drwa.game.service;
 
 import com.a708.drwa.game.data.dto.request.GameInfoCreateRequestDto;
-import com.a708.drwa.game.data.dto.request.RecordCreateRequestDto;
+import com.a708.drwa.game.data.dto.request.GameInfoCreateRequestRedisDto;
+import com.a708.drwa.game.data.dto.request.RecordRequestDto;
+import com.a708.drwa.game.data.dto.request.RecordRequestRedisDto;
+import com.a708.drwa.game.data.dto.response.GameInfoCreateResponseDto;
+import com.a708.drwa.game.data.dto.response.GameInfoCreateResponseRedisDto;
+import com.a708.drwa.game.data.dto.response.RecordResponseDto;
+import com.a708.drwa.game.data.dto.response.RecordResponseRedisDto;
 import com.a708.drwa.game.domain.GameInfo;
 import com.a708.drwa.game.domain.Record;
+import com.a708.drwa.game.domain.Result;
+import com.a708.drwa.game.domain.Team;
 import com.a708.drwa.game.repository.GameInfoRepository;
 import com.a708.drwa.game.repository.RecordRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,28 +24,49 @@ public class GameService {
 
     private final GameInfoRepository gameInfoRepository;
     private final RecordRepository recordRepository;
+    private final int pointWeight = 1;      // 1투표 당 포인트
+    private final int pointMvpWeight = 5;   // MVP로 선정되었을 시 가산점
 
-    // 정산 시점에 몇명이 있든 단 한번만 실행되어야 한다.(프론트 단에서 Host만 수행하도록 하면 되나?)
+    // Host가 토론 시작을 누르면 수행됨
     @Transactional
-    public GameInfo createGameInfo(GameInfoCreateRequestDto gameInfoRequestDto) {
-        GameInfo gameInfo = GameInfo.builder()
-                .keyword(gameInfoRequestDto.getKeyword())
-                .mvpMemberId(gameInfoRequestDto.getMvpMemberId())
-                .build();
-
-        return gameInfoRepository.save(gameInfo);
+    public GameInfoCreateResponseDto createGameInfo(GameInfoCreateRequestDto gameInfoCreateRequestDto) {
+        GameInfo savedGameInfo = gameInfoRepository.save(GameInfo.builder()
+                        .keyword(gameInfoCreateRequestDto.)
+                .build());
     }
 
-    // 정산 시점에 모든 토론자와 배심원이 수행한다.햣
+    // 전적 생성 => 모든 참여자
     @Transactional
-    public Record createRecord(RecordCreateRequestDto recordRequestDto) {
-        Record record = Record.builder()
-                .memberId(recordRequestDto.getMemberId())
-                .gameId(recordRequestDto.getGameId())
-                .result(recordRequestDto.getResult())
-                .team(recordRequestDto.getTeam())
+    public RecordResponseDto createRecord(RecordRequestDto recordRequestDto) {
+        // Redis 통신을 위한 Request DTO 초기화
+        RecordRequestRedisDto recordRequestRedisDto = RecordRequestRedisDto
+                .builder()
+                .debateId(recordRequestDto.getDebateId())
                 .build();
 
-        return recordRepository.save(record);
+        // Redis 통신 결과
+        RecordResponseRedisDto recordResponseRedisDto
+                = recordRepository.getRecordResponseRedisDto(recordRequestRedisDto);
+
+        // MySQL 통신
+        Result result;
+        Team team;
+        if(recordResponseRedisDto.getTeamAList().)
+        Record savedRecord = recordRepository.save(Record.builder()
+                        .gameId(recordRequestDto.getGameId())
+                        .memberId(recordRequestDto.getMemberId())
+                        .result()
+                        .team()
+                .build());
+        GameInfo savedGameInfo = gameInfoRepository.save(GameInfo.builder()
+                .keyword(gameInfoCreateResponseRedisDto.getKeyword())
+                .build());
+
+        // 클라이언트에게 전달할 데이터 반환
+        return GameInfoCreateResponseDto.builder()
+                .gameId(savedGameInfo.getGameId())
+                .build();
     }
+
+    private int
 }
