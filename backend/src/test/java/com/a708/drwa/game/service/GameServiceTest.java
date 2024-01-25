6827aac72896b63c03bf669b4a5c8acb5c6076d8
jjh <jjho311@naver.com>
2024-01-25 17:14:59 +0900

    Test : 게임 서비스 테스트 템플릿

---
+++ backend/src/test/java/com/a708/drwa/game/service/GameServiceTest.java
@@ -0,0 +1,15 @@
+package com.a708.drwa.game.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class GameServiceTest {
+
+    @Test
+    public void redisSelectTest() {
+
+    }
+}
