09642b3afbed7df46bc574a75d3517bf92ae7baf
jjh <jjho311@naver.com>
2024-01-25 16:56:46 +0900

    Feat : Redis에서 List 데이터를 받아오는 메서드 추가

--- backend/src/main/java/com/a708/drwa/redis/util/RedisUtil.java
+++ backend/src/main/java/com/a708/drwa/redis/util/RedisUtil.java
@@ -4,12 +4,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @Service
 @RequiredArgsConstructor
 public class RedisUtil {
     private final RedisTemplate<String, Object> redisTemplate;
 
-    public void setDate(String key, String value, Long expiredTime) {
-
+    public void setData(String key, String value, Long expiredTime) {
+        redisTemplate.opsForValue().set(key, value, expiredTime, TimeUnit.MILLISECONDS);
     }
+
+    public String getData(String key) {
+        return String.valueOf(redisTemplate.opsForValue().get(key));
+    }
+
+    public List<Object> getListData(String key) {
+        return redisTemplate.opsForList().range(key, 0, -1);
+    }
+
+    public void deleteData(String key) {
+        redisTemplate.delete(key);
+    }
+
 }
