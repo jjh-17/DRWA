e485880e7aac5777b601b549fda099f2099aee38
jjh <jjho311@naver.com>
2024-01-25 17:13:32 +0900

    Refactor : redis 통신 메서드를 Service로 옮김

--- backend/src/main/java/com/a708/drwa/game/repository/GameInfoRepository.java
+++ backend/src/main/java/com/a708/drwa/game/repository/GameInfoRepository.java
@@ -2,6 +2,10 @@ package com.a708.drwa.game.repository;
 
 import com.a708.drwa.game.domain.GameInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GameInfoRepository extends JpaRepository<GameInfo, Integer> {
+    // MVP 선정 멤버 ID 수정
+    @Query("UPDATE GameInfo g SET g.mvpMemberId=:memberId WHERE g.gameId=:gameId")
+    int updateMvp(int gameId, int memberId);
 }
