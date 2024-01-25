734348763a43b44b241e8ea98d5427590b868581
jjho311 <jjho311@naver.com>
2024-01-25 22:45:13 +0900

    Remove : 게임 정보 MVP 멤버 Id 수정 메서드 삭제

--- backend/src/main/java/com/a708/drwa/game/repository/GameInfoRepository.java
+++ backend/src/main/java/com/a708/drwa/game/repository/GameInfoRepository.java
@@ -2,10 +2,8 @@ package com.a708.drwa.game.repository;
 
 import com.a708.drwa.game.domain.GameInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GameInfoRepository extends JpaRepository<GameInfo, Integer> {
-    // MVP 선정 멤버 ID 수정
-    @Query("UPDATE GameInfo g SET g.mvpMemberId=:memberId WHERE g.gameId=:gameId")
-    int updateMvp(int gameId, int memberId);
 }
