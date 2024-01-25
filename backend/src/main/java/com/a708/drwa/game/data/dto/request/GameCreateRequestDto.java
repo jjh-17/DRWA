d297bf29acd92af3d5c030f831daadfa3c2db486
jjh <jjho311@naver.com>
2024-01-25 16:49:40 +0900

    Feat : 전적, 게임 정보 저장을 위한 통합 요청 DTO

---
+++ backend/src/main/java/com/a708/drwa/game/data/dto/request/GameCreateRequestDto.java
@@ -0,0 +1,10 @@
+package com.a708.drwa.game.data.dto.request;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class GameCreateRequestDto {
+    private int debateId;
+}
