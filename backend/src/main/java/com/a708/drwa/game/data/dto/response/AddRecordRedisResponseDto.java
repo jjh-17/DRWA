d5868fc2ac0a693ac8e2f3798a983804bd8b2cf2
jjho311 <jjho311@naver.com>
2024-01-25 22:41:12 +0900

    Rename : 이름 수정

--- backend/src/main/java/com/a708/drwa/game/data/dto/response/AddRecordRedisResponseDto.java
+++ backend/src/main/java/com/a708/drwa/game/data/dto/response/AddRecordRedisResponseDto.java
@@ -1,17 +1,20 @@
-package com.a708.drwa.game.service;
+package com.a708.drwa.game.data.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
 import java.util.List;
 
+// 전적 저장을 위해 Redis에서 받아올 정보
 @Builder
 @Getter
-public class RedisGameResponseDto {
-    private List<Object> teamAList;
-    private List<Object> teamBList;
-    private List<Object> jurorList;
-    private List<Object> viewerList;
-    private int teamAVoteNum;
-    private int teamBVoteNum;
-    private String keyword;
+public class AddRecordRedisResponseDto {
+    private List<Object> teamAList;     // A팀 리스트
+    private List<Object> teamBList;     // B팀 리스트
+    private List<Object> jurorList;     // 배심원 리스트
+    private List<Object> viewerList;    // 관전자 리스트
+    
+    private int teamAVoteNum;           // 팀A 투표 수 
+    private int teamBVoteNum;           // 팀B 투표 수
+    
+    private String keyword;             // 키워드
 }
