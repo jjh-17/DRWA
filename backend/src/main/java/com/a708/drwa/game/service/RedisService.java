4b8766203be7a839e729e9f1f9126f5dc49ef96e
jjho311 <jjho311@naver.com>
2024-01-25 22:47:07 +0900

    Refactor : 편의 메서드 추가 및 코드 간소화

--- backend/src/main/java/com/a708/drwa/game/service/RedisService.java
+++ backend/src/main/java/com/a708/drwa/game/service/RedisService.java
@@ -5,7 +5,6 @@ import com.a708.drwa.game.data.dto.response.AddRecordRedisResponseDto;
 import com.a708.drwa.game.data.dto.response.WinnerTeam;
 import com.a708.drwa.game.domain.GameInfo;
 import com.a708.drwa.game.domain.Record;
-import com.a708.drwa.game.repository.GameInfoRepository;
 import com.a708.drwa.game.repository.RecordRepository;
 import com.a708.drwa.redis.util.RedisKeyUtil;
 import com.a708.drwa.redis.util.RedisUtil;
@@ -18,51 +17,19 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class GameService {
+public class RedisService {
 
-    private final GameInfoRepository gameInfoRepository;
     private final RecordRepository recordRepository;
     private final RedisUtil redisUtil;
-    private final RedisKeyUtil redisKeyUtil = new RedisKeyUtil();
-
-    // MySql에 전적 저장 서비스
-    @Transactional
-    public void addRecord(List<Record> records) {
-        //
-    }
-
-    // Redis에
-
-    // 전적 정보 저장 서비스
-
-    // 게임 정보의 mvpMemberId 수정 서비스
-
+    private static final RedisKeyUtil redisKeyUtil = new RedisKeyUtil();
+    private static final int mvpPoint = 30;
+    private static final int winnerPoint = 100;
+    private AddRecordRedisResponseDto addRecordRedisResponseDto;
 
     @Transactional
-    public AddRecordResponseDto createGame(int debateId) {
-        // 필요한 데이터 정의
-        final AddRecordRedisResponseDto addRecordRedisResponseDto;
-        final int mvpPoint = 50;
-        final int winnerPoint = 30;
-        int noVoteNum;
-        int mvpMemberId;
-        int gameId;
-        WinnerTeam winnerTeam;  // 승리 팀
-
+    public AddRecordResponseDto addRecord(int debateId) {
         // Redis 통신
         addRecordRedisResponseDto = getRedisGameResponseDto(debateId);
-
-        // 데이터 연산
-        winnerTeam = getWinnerTeam(addRecordRedisResponseDto.getTeamAVoteNum(), addRecordRedisResponseDto.getTeamBVoteNum());
-        noVoteNum = addRecordRedisResponseDto.getJurorList().size() + addRecordRedisResponseDto.getViewerList().size()
-                - addRecordRedisResponseDto.getTeamAVoteNum() - addRecordRedisResponseDto.getTeamBVoteNum();
-        mvpMemberId = getMVP(addRecordRedisResponseDto.getTeamAList(), addRecordRedisResponseDto.getTeamBList());
-
-        // MySql에 게임 정보 저장
-        gameId = gameInfoRepository.save(GameInfo.builder()
-                        .keyword(addRecordRedisResponseDto.getKeyword())
-                        .mvpMemberId(mvpMemberId)
-                .build()).getGameId();
 
         // MySql에 전적 저장
         List<Record> records = getInputRecords();
@@ -71,45 +38,48 @@ public class GameService {
         return AddRecordResponseDto.builder()
                 .teamAVoteNum(addRecordRedisResponseDto.getTeamAVoteNum())
                 .teamBVoteNum(addRecordRedisResponseDto.getTeamBVoteNum())
-                .noVoteNum(noVoteNum)
-                .mvpMemberId(mvpMemberId)
+                .noVoteNum(getNoVoteNum())
+                .mvpMemberId(getMvpMemberId())
                 .mvpPoint(mvpPoint)
                 .winnerPoint(winnerPoint)
-                .winnerTeam(winnerTeam)
+                .winnerTeam(getWinnerTeam())
                 .build();
     }
 
-    // 레디스와 통신하여 필요한 데이터 받아옴
+    // 레디스 통신
     private AddRecordRedisResponseDto getRedisGameResponseDto(int debateId) {
         return AddRecordRedisResponseDto.builder()
                 .build();
     }
 
     // === 편의 메서드 ===
-    private List<Record> getInputRecords() {
-        List<Record> records = new ArrayList<>();
-
-        return records;
+    // 미투표 수 도출 메서드
+    private int getNoVoteNum() {
+        return addRecordRedisResponseDto.getJurorList().size() + addRecordRedisResponseDto.getViewerList().size()
+                - addRecordRedisResponseDto.getTeamAVoteNum() - addRecordRedisResponseDto.getTeamBVoteNum();
     }
 
-    // 승리 팀 반환
-    private WinnerTeam getWinnerTeam(int voteTeamA, int voteTeamB) {
-        if(voteTeamA > voteTeamB)       return WinnerTeam.A;
-        else if(voteTeamA < voteTeamB)  return WinnerTeam.B;
-        else                            return WinnerTeam.TIE;
+    // 승리 팀 도출
+    private WinnerTeam getWinnerTeam() {
+        if(addRecordRedisResponseDto.getTeamAVoteNum() > addRecordRedisResponseDto.getTeamBVoteNum())       return WinnerTeam.A;
+        else if(addRecordRedisResponseDto.getTeamAVoteNum() < addRecordRedisResponseDto.getTeamBVoteNum())  return WinnerTeam.B;
+        else                                                                                                return WinnerTeam.TIE;
     }
 
-    // MVP 반환
-    private int getMVP(List<Object> teamAList, List<Object> teamBList) {
+    // MVP 도출
+    private int getMvpMemberId() {
         int mvpMemberId = -1;
         boolean isSame = true;
         int maxVote = 0;
 
-        for(Iterator<Object> iter = teamAList.iterator(); iter.hasNext();) {
-            int memberId = (int) iter.next();
-
-        }
+        // MVP 에서
 
         return mvpMemberId;
+    }
+
+    private static List<Record> getInputRecords() {
+        List<Record> records = null;
+
+        return records;
     }
 }
