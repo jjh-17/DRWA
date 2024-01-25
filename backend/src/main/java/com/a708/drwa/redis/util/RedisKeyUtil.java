a682fd31724112a8a027cbc8eff11d3559835a09
jjh <jjho311@naver.com>
2024-01-25 16:55:42 +0900

    Feat : memberId를 포함하는 key 생성 메서드 추가

--- backend/src/main/java/com/a708/drwa/redis/util/RedisKeyUtil.java
+++ backend/src/main/java/com/a708/drwa/redis/util/RedisKeyUtil.java
@@ -6,9 +6,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class RedisKeyUtil {
 
+    // 일반 키 값
     public String getKeyByDebateIdWithKeyword(int debateId, DebateRedisKey key) {
         return "debate:" + debateId + key;
     }
 
+    // MVP와 같이 key에 멤버 정보 또한 필요한 경우
+    public String getKeyByDebateIdMemberIdWithKeyword(int debateId, int memberId, DebateRedisKey key) {
+        return "debate:" + debateId + "member:" + memberId + key;
+    }
 
 }
