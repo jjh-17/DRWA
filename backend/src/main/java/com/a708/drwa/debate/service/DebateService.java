cedd21f87019c2f3050f7b90c033a59ec2400753
lkt9899 <lkt9899@naver.com>
2024-01-26 16:49:35 +0900

    Style : 방 생성 서비스 수정

--- backend/src/main/java/com/a708/drwa/debate/service/DebateService.java
+++ backend/src/main/java/com/a708/drwa/debate/service/DebateService.java
@@ -2,13 +2,12 @@ package com.a708.drwa.debate.service;
 
 import com.a708.drwa.debate.data.dto.request.DebateCreateRequestDto;
 import com.a708.drwa.debate.data.dto.request.DebateJoinRequestDto;
+import com.a708.drwa.debate.domain.Debate;
+import com.a708.drwa.debate.domain.DebateCategory;
 import com.a708.drwa.debate.exception.DebateErrorCode;
 import com.a708.drwa.debate.exception.DebateException;
+import com.a708.drwa.debate.repository.DebateCategoryRepository;
 import com.a708.drwa.debate.repository.DebateRepository;
-import io.openvidu.java.client.OpenVidu;
-import io.openvidu.java.client.OpenViduHttpException;
-import io.openvidu.java.client.OpenViduJavaClientException;
-import io.openvidu.java.client.Session;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,17 +17,29 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class DebateService {
     private final DebateRepository debateRepository;
+    private final DebateCategoryRepository debateCategoryRepository;
 
+    /**
+     * 토론 방 생성
+     * @param debateCreateRequestDto
+     * @return
+     */
     @Transactional
     public int create(DebateCreateRequestDto debateCreateRequestDto) {
-        return debateRepository.save(debateCreateRequestDto.toEntity()).getDebateId();
+        // 카테고리 가져옴
+        DebateCategory debateCategory = debateCategoryRepository
+                .findById(debateCreateRequestDto.getDebateCategoryId())
+                .orElse(null);
+
+        return debateRepository.save(Debate.builder().debateCategory(debateCategory).build())
+                .getDebateId();
     }
+
 
     public Boolean isExistDebate(DebateJoinRequestDto debateJoinRequestDto) {
         // 존재하지 않는 방일 경우
         if(!debateRepository.existsById(debateJoinRequestDto.getDebateId()))
             throw new DebateException(DebateErrorCode.NOT_EXIST_DEBATE_ROOM_ERROR);
         return true;
-
     }
 }
