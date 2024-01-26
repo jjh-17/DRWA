2ccefc4b9f1571173bd2e44b2be96714527b69a9
jjh <jjho311@naver.com>
2024-01-26 17:23:45 +0900

    Refactor : mvp를 리스트 형식으로 반환하도록 수정

--- backend/src/main/java/com/a708/drwa/game/data/dto/response/AddRecordResponseDto.java
+++ backend/src/main/java/com/a708/drwa/game/data/dto/response/AddRecordResponseDto.java
@@ -1,19 +1,25 @@
 package com.a708.drwa.game.data.dto.response;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 // 전적 저장 후 클라이언트에게 전달할 데이터
 @Builder
 @Getter
 public class AddRecordResponseDto {
-    private int teamAVoteNum;       // 팀A 투표 수
-    private int teamBVoteNum;       // 팀B 투표 수
-    private int noVoteNum;          // 미투표 수
+    private int teamAVoteNum;               // 팀A 투표 수
+    private int teamBVoteNum;               // 팀B 투표 수
+    private int noVoteNum;                  // 미투표 수
 
-    private int mvpMemberId;        // MVP로 선정된 멤버 ID
-    private int mvpPoint;           // MVP 추가 포인트
+    private List<Integer> mvpMemberIds;     // MVP로 선정된 멤버 ID
+    private int mvpPoint;                   // MVP 추가 포인트
 
-    private int winnerPoint;        // 승리팀 획득 포인트
-    private int winnerTeam;         // 승리 팀
+    private int winnerPoint;                // 승리팀 획득 포인트
+
+    @Enumerated(EnumType.STRING)
+    private WinnerTeam winnerTeam;          // 승리 팀
 }
