4b6dfb3a1b2cbe333656c63f7d60474247248b5c
jjh <jjho311@naver.com>
2024-01-26 11:28:52 +0900

    Refactor : winnerTeam 타입 변경

--- backend/src/main/java/com/a708/drwa/game/data/dto/response/AddRecordResponseDto.java
+++ backend/src/main/java/com/a708/drwa/game/data/dto/response/AddRecordResponseDto.java
@@ -1,7 +1,5 @@
 package com.a708.drwa.game.data.dto.response;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,10 +12,8 @@ public class AddRecordResponseDto {
     private int noVoteNum;          // 미투표 수
 
     private int mvpMemberId;        // MVP로 선정된 멤버 ID
-    
     private int mvpPoint;           // MVP 추가 포인트
+
     private int winnerPoint;        // 승리팀 획득 포인트
-    
-    @Enumerated(EnumType.ORDINAL)
-    private WinnerTeam winnerTeam;  // 승리 팀
+    private int winnerTeam;         // 승리 팀
 }
