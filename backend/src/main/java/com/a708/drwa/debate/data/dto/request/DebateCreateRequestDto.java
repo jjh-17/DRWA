501c584b986b4c02dc205b21513a99fde06971cd
lkt9899 <lkt9899@naver.com>
2024-01-26 16:46:59 +0900

    Feat : 토론 방 생성 Dto

--- backend/src/main/java/com/a708/drwa/debate/data/dto/request/DebateCreateRequestDto.java
+++ backend/src/main/java/com/a708/drwa/debate/data/dto/request/DebateCreateRequestDto.java
@@ -1,19 +1,35 @@
 package com.a708.drwa.debate.data.dto.request;
 
 import com.a708.drwa.debate.domain.Debate;
-import com.a708.drwa.debate.domain.DebateCategory;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
 public class DebateCreateRequestDto {
-    private DebateCategory debateCategory;
+    // 토론 카테고리
+    private int debateCategoryId;
 
-    public Debate toEntity() {
-        return Debate.builder()
-                .debateCategory(debateCategory)
-                .build();
-    }
+    // 토론 방 제목
+    private String debateTitle;
+
+    // 제시어 1, 2
+    private String leftKeyword;
+    private String rightKeyword;
+
+    // 참여 인원 제한
+    private int peopleCnt;
+
+    // 비공개 시 비번 설정
+    private Boolean isPrivate;
+    private String password;
+
+    // 페이즈 별 시간
+    private int speakingTime;
+    private int readyTime;
+    private int qnaTime;
+
+    // 썸네일
+    private String thumbnail1;
+    private String thumbnail2;
 }
