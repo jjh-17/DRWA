e82d15ef22e1422cd3e0a2670e5173a94eb2d55b
lkt9899 <lkt9899@naver.com>
2024-01-26 13:12:47 +0900

    Refactor : 방 참여 기능 통합

--- backend/src/main/java/com/a708/drwa/debate/service/DebateService.java
+++ backend/src/main/java/com/a708/drwa/debate/service/DebateService.java
@@ -5,11 +5,13 @@ import com.a708.drwa.debate.data.dto.request.DebateJoinRequestDto;
 import com.a708.drwa.debate.exception.DebateErrorCode;
 import com.a708.drwa.debate.exception.DebateException;
 import com.a708.drwa.debate.repository.DebateRepository;
+import com.a708.drwa.redis.util.RedisUtil;
 import io.openvidu.java.client.OpenVidu;
 import io.openvidu.java.client.OpenViduHttpException;
 import io.openvidu.java.client.OpenViduJavaClientException;
 import io.openvidu.java.client.Session;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.apache.bcel.classfile.Module;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,17 +20,25 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class DebateService {
     private final DebateRepository debateRepository;
+    private final RedisUtil redisUtil;
 
     @Transactional
     public int create(DebateCreateRequestDto debateCreateRequestDto) {
         return debateRepository.save(debateCreateRequestDto.toEntity()).getDebateId();
     }
 
+//    public String join(DebateJoinRequestDto debateJoinRequestDto) {
+//
+//    }
+
     public Boolean isExistDebate(DebateJoinRequestDto debateJoinRequestDto) {
         // 존재하지 않는 방일 경우
         if(!debateRepository.existsById(debateJoinRequestDto.getDebateId()))
             throw new DebateException(DebateErrorCode.NOT_EXIST_DEBATE_ROOM_ERROR);
         return true;
+    }
+
+    public void nextPhase(int debateId) {
 
     }
 }
