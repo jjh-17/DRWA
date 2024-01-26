c7d2343ac012178615256b5b1672f28bf1d6f22e
jjh <jjho311@naver.com>
2024-01-26 11:30:05 +0900

    Feat : 오류 코드 추가

--- backend/src/main/java/com/a708/drwa/game/exception/GameErrorCode.java
+++ backend/src/main/java/com/a708/drwa/game/exception/GameErrorCode.java
@@ -11,7 +11,9 @@ public enum GameErrorCode implements ErrorCode {
     BAD_REQUEST(400, "Bad Request", "필수 정보가 누락되었거나 형식과 다른 데이터를 요청하셨습니다."),
     UNAUTHORIZED(401, "Unauthorized", "사용자 인증 정보를 찾을 수 없습니다."),
     FORBIDDEN(403, "Forbidden", "해당 요청은 수행할 수 없습니다."),
-    NOT_FOUND(404, "Not Found", "존재하지 않는 정보입니다.");
+    DEBATE_NOT_FOUND(404, "Debate Not Found", "토론을 찾을 수 없습니다."),
+    GAME_NOT_FOUND(404, "Game Not Found", "개임을 찾을 수 없습니다.");
+
 
     private final int statusCode;
     private final String errorCode;
