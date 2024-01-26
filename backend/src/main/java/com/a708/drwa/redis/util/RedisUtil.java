d1895f962bc85b14068e70be7ab5fc89a95f7672
jjh <jjho311@naver.com>
2024-01-26 17:31:16 +0900

    Feat : Redis에서 데이터를 받아오기 위한 편의 메서드 추가

--- backend/src/main/java/com/a708/drwa/redis/util/RedisUtil.java
+++ backend/src/main/java/com/a708/drwa/redis/util/RedisUtil.java
@@ -1,16 +1,18 @@
 package com.a708.drwa.redis.util;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 public class RedisUtil {
     private final RedisTemplate<String, Object> redisTemplate;
+    private final RedisTemplate<String, Integer> integerRedisTemplate;
 
     public void setData(String key, String value, Long expiredTime) {
         redisTemplate.opsForValue().set(key, value, expiredTime, TimeUnit.MILLISECONDS);
@@ -20,8 +22,17 @@ public class RedisUtil {
         return String.valueOf(redisTemplate.opsForValue().get(key));
     }
 
-    public List<Object> getListData(String key) {
-        return redisTemplate.opsForList().range(key, 0, -1);
+    public Integer getIntegerData(String key) {
+        return Integer.parseInt(String.valueOf(redisTemplate.opsForValue().get(key)));
+    }
+
+    public List<Integer> getIntegerListData(String key) {
+        return integerRedisTemplate.opsForList().range(key, 0, -1);
+    }
+
+    public Map<Integer, Integer> getIntegerIntegerMapData(String key) {
+        HashOperations<String, Integer, Integer> hashOperations = redisTemplate.opsForHash();
+        return hashOperations.entries(key);
     }
 
     public void deleteData(String key) {
