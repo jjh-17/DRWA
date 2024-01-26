4d19b6ca39bb04104cc3b054bb2bc5e583317dac
jjh <jjho311@naver.com>
2024-01-26 11:29:35 +0900

    Refactor : 각 요소에 값 지정, getter, 생성자 추가

--- backend/src/main/java/com/a708/drwa/game/domain/Team.java
+++ backend/src/main/java/com/a708/drwa/game/domain/Team.java
@@ -1,6 +1,11 @@
 package com.a708.drwa.game.domain;
 
+import lombok.RequiredArgsConstructor;
+
 // 참여팀 : 배심원, 팀A, 팀B
+@RequiredArgsConstructor
 public enum Team {
-    JUROR, A, B
+    JUROR(0), A(1), B(2);
+
+    private final int team;
 }
