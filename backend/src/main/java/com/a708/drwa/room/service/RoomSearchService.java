205b9072c38501de29193396f087208cfe74d075
noowadc <cha12608@naver.com>
2024-01-28 23:22:53 +0900

    Refactor: 검색 결과 향상을 위해 RoomSearchService에 Redis 통합

--- backend/src/main/java/com/a708/drwa/room/service/RoomSearchService.java
+++ backend/src/main/java/com/a708/drwa/room/service/RoomSearchService.java
@@ -1,48 +1,48 @@
 package com.a708.drwa.room.service;
 
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
 import com.a708.drwa.room.domain.Room;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
 public class RoomSearchService {
     @Autowired
     private ElasticsearchClient elasticsearchClient;
+
     @Autowired
-    private RoomRedisService roomRedisService;
+    private RedisTemplate<String, Room> redisTemplate;
 
     public List<Room> searchRoomsByNori(String query) {
-        Query searchQuery = Query.of(q -> q
-                .match(m -> m
-                        .field("title")
-                        .query(query)
-                        .analyzer("nori")
+        SearchRequest request = new SearchRequest.Builder()
+                .index("room_index")
+                .query(q -> q
+                        .match(m -> m
+                                .field("title")
+                                .query(query)
+                                .analyzer("nori")
+                        )
                 )
-        );
-        SearchRequest searchRequest = new SearchRequest.Builder()
-                .index("rooms")
-                .query(searchQuery)
                 .build();
 
-
         try {
-            SearchResponse<Room> searchResponse = elasticsearchClient.search(searchRequest, Room.class);
-            // Elasticsearch 검색 결과 처리
-            return searchResponse.hits().hits().stream()
-                    .map(hit -> roomRedisService.getRoomFromRedis(hit.id()))
-                    .filter(Objects::nonNull)
+            SearchResponse<Room> response = elasticsearchClient.search(request, Room.class);
+            return response.hits().hits().stream()
+                    .map(hit -> redisTemplate.opsForValue().get(hit.id())) // Redis에서 ID를 키로 사용하여 데이터 조회
+                    .filter(room -> room != null) // null이 아닌 결과만 필터링
                     .collect(Collectors.toList());
-        } catch (ElasticsearchException e) {
+        } catch (Exception e) {
             return Collections.emptyList();
         }
     }
-
-
-
 }
+
+
