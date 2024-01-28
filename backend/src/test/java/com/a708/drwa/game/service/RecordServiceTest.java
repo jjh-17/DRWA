093c2f93ca6a569a446a1f4b938632ebdc25e502
jjho311 <jjho311@naver.com>
2024-01-28 23:41:42 +0900

    Test : MVP 리스트 반환 메서드 테스트 추가

--- backend/src/test/java/com/a708/drwa/game/service/RecordServiceTest.java
+++ backend/src/test/java/com/a708/drwa/game/service/RecordServiceTest.java
@@ -1,15 +1,28 @@
 package com.a708.drwa.game.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 @Transactional
 class RecordServiceTest {
 
-    @Test
-    public void redisSelectTest() {
+    @Autowired
+    RecordService recordService;
 
+    @Test
+    public void getMvpListTest() {
+        Map<Integer, Integer> mvpMap = new HashMap<>();
+        mvpMap.put(1, 2);
+        mvpMap.put(2, 2);
+        mvpMap.put(3, 5);
+        mvpMap.put(4, 3);
+
+        System.out.println(recordService.getMvpList(mvpMap));
     }
 }
