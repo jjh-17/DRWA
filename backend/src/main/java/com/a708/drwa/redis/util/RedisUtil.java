131d40ce807d983e31e0e83afc4de368e4464942
jjh <jjho311@naver.com>
2024-01-29 17:44:30 +0900

    Feat : List, Map 데이터를 Redis에 추가/검색하는 메서드와 에러 처리 추가

--- backend/src/main/java/com/a708/drwa/redis/util/RedisUtil.java
+++ backend/src/main/java/com/a708/drwa/redis/util/RedisUtil.java
@@ -1,44 +1,66 @@
 package com.a708.drwa.redis.util;
 
+import com.a708.drwa.redis.Exception.RedisErrorCode;
+import com.a708.drwa.redis.Exception.RedisException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+
+import java.sql.Time;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class RedisUtil {
     private final RedisTemplate<String, Object> redisTemplate;
 
-    public void setData(String key, String value, Long expiredTime) {
+    public void setData(String key, Object value, Long expiredTime) {
         redisTemplate.opsForValue().set(key, value, expiredTime, TimeUnit.MILLISECONDS);
+    }
+
+    public void setListData(String key, List<Object> list, int expiredTime) {
+        ListOperations<String, Object> stringObjectListOperations = redisTemplate.opsForList();
+        for(Object l : list)
+            stringObjectListOperations.rightPush(key, l);
+        redisTemplate.expire(key, expiredTime, TimeUnit.MINUTES);
+    }
+
+    public void setMapData(String key, Map<Object, Object> map, int expiredTime) {
+        redisTemplate.opsForHash().putAll(key, map);
+        redisTemplate.expire(key, expiredTime, TimeUnit.MINUTES);
     }
 
     public String getData(String key) {
         return String.valueOf(redisTemplate.opsForValue().get(key));
     }
 
-    public Integer getIntegerData(String key) {
-        return Integer.parseInt(String.valueOf(redisTemplate.opsForValue().get(key)));
+    public int getIntegerData(String key) {
+        String result = String.valueOf(redisTemplate.opsForValue().get(key));
+        if(result==null) throw new RedisException(RedisErrorCode.REDIS_SELECT_FAIL);
+        return Integer.parseInt(result);
     }
 
-    public List<Integer> getIntegerListData(String key) {
-        return Objects.requireNonNull(redisTemplate.opsForList().range(key, 0, -1))
-                .stream().map(op -> (int) op).toList();
+    public List<Object> getListData(String key) {
+        List<Object> result = redisTemplate.opsForList().range(key, 0, -1);
+        if(result==null || result.isEmpty()) throw new RedisException(RedisErrorCode.REDIS_SELECT_FAIL);
+        return result;
     }
 
-    public Map<Integer, Integer> getIntegerIntegerMapData(String key) {
-        HashOperations<String, Integer, Integer> hashOperations = redisTemplate.opsForHash();
-        return hashOperations.entries(key);
+    public Map<Object, Object> getMapData(String key) {
+        Map<Object, Object> result = redisTemplate.opsForHash().entries(key);
+        if(result.isEmpty()) throw new RedisException(RedisErrorCode.REDIS_SELECT_FAIL);
+        return result;
     }
 
     public void deleteData(String key) {
         redisTemplate.delete(key);
+    }
+
+    public Long deleteDataAll(List<String> keys) {
+        return redisTemplate.delete(keys);
     }
 
 }
