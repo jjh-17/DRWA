a79f758dc7ae8b9dabfa29aa4fb22eff8ef19241
KimJihwan98 <wlghks98k@gmail.com>
2024-01-29 17:45:49 +0900

    Refactor : 토론 방 생성 시 보내는 요청 수정

--- backend/src/main/java/com/a708/drwa/debate/data/dto/request/DebateCreateRequestDto.java
+++ backend/src/main/java/com/a708/drwa/debate/data/dto/request/DebateCreateRequestDto.java
@@ -1,19 +1,39 @@
 package com.a708.drwa.debate.data.dto.request;
 
-import com.a708.drwa.debate.domain.Debate;
 import com.a708.drwa.debate.domain.DebateCategory;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@Getter
+@Data
 public class DebateCreateRequestDto {
     private DebateCategory debateCategory;
 
-    public Debate toEntity() {
-        return Debate.builder()
-                .debateCategory(debateCategory)
-                .build();
-    }
+    // 토론 id
+    private int debateId;
+
+    // 현재 단계 방 생성 : 0단계
+    private int phase;
+
+    // 제목 , 제시어, 방장
+    private int title;
+    private int keywordA;
+    private int keywordB;
+    private int host;
+
+    // 공개방? 비밀방?
+    private boolean isPrivate;
+    private int password;
+
+    // 플레이어수, 배심원수
+    private int playerNum;
+    private int jurorNum;
+
+    // 준비시간, 발언시간, 질문시간
+    private int opinionTime;
+    private int readyTime;
+    private int questionTime;
+
+    // 방에 들어와있는 총 인원수 ->  생성시 :0명
+    private int totalNum;
 }
