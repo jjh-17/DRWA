9f07f0383687252ae5632e6327c9a0f628b7cc4b
minwoo-kwak <minwoo.kwak540@gmail.com>
2024-01-29 10:28:06 +0900

    Feat : 리프레시 토큰 silent-refresh 기능 추가 access token 만료시 refresh 토큰으로 재발급하는 기능입니다. redis와 서버 오류시의 예외처리를 해야하는 지 알고 싶습니다.

--- backend/src/main/java/com/a708/drwa/member/service/AuthService.java
+++ backend/src/main/java/com/a708/drwa/member/service/AuthService.java
@@ -1,7 +1,12 @@
 package com.a708.drwa.member.service;
 
-import org.springframework.stereotype.Service;
 
-@Service
-public class AuthService {
+import com.a708.drwa.member.dto.AuthDto;
+
+public interface AuthService {
+    void setRefreshToken(AuthDto authDto, long duration);
+    AuthDto getRefreshToken(String userId);
+    void updateRefreshToken(AuthDto authDto, long duration);
+
+
 }
