91e2103aa2e1d570e6365ecfc010368534909715
KimJihwan98 <wlghks98k@gmail.com>
2024-01-29 17:47:23 +0900

    Feat : 방 생성 시 방장 입장 기능 추가

--- backend/src/main/java/com/a708/drwa/debate/controller/DebateController.java
+++ backend/src/main/java/com/a708/drwa/debate/controller/DebateController.java
@@ -4,7 +4,14 @@ import com.a708.drwa.debate.data.dto.request.DebateCreateRequestDto;
 import com.a708.drwa.debate.data.dto.request.DebateJoinRequestDto;
 import com.a708.drwa.debate.service.DebateService;
 import com.a708.drwa.debate.service.OpenViduService;
+import com.a708.drwa.redis.domain.DebateRedisKey;
+import com.a708.drwa.redis.util.RedisKeyUtil;
+import com.a708.drwa.room.domain.Room;
+import io.openvidu.java.client.OpenViduHttpException;
+import io.openvidu.java.client.OpenViduJavaClientException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class DebateController {
     private final DebateService debateService;
     private final OpenViduService openViduService;
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final RedisKeyUtil redisKeyUtil;
 
     /**
      * 방 생성 API
@@ -28,11 +37,24 @@ public class DebateController {
      */
     @PostMapping("/create")
     public ResponseEntity<Void> create(@RequestBody DebateCreateRequestDto debateCreateRequestDto) {
+        int debateId = debateCreateRequestDto.getDebateId();
         openViduService.create(debateService.create(debateCreateRequestDto));
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        String hostKey = redisKeyUtil.getKeyByDebateIdWithKeyword(debateId, DebateRedisKey.HOST);
+        int hostId = (int) valueOperations.get(hostKey);
+
+        DebateJoinRequestDto debateJoinRequestDto = new DebateJoinRequestDto();
+        debateJoinRequestDto.setDebateId(debateId);
+        debateJoinRequestDto.setUserId(hostId);
+
+        String token = openViduService.join(debateJoinRequestDto.getDebateId());
+
+
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .build();
     }
+
 
     /**
      * 방 입장 API
