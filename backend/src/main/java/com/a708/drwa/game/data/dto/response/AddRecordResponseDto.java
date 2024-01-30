dfab937e1ea99013a5426e3bcc712bd40f200db3
jjh <jjho311@naver.com>
2024-01-30 10:47:23 +0900

    Refactor : 코드 깔끔하게 정리

--- backend/src/main/java/com/a708/drwa/game/data/dto/response/AddRecordResponseDto.java
+++ backend/src/main/java/com/a708/drwa/game/data/dto/response/AddRecordResponseDto.java
@@ -1,10 +1,7 @@
 package com.a708.drwa.game.data.dto.response;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.Builder;
 import lombok.Getter;
-
 import java.util.List;
 
 // 전적 저장 후 클라이언트에게 전달할 데이터
@@ -19,7 +16,5 @@ public class AddRecordResponseDto {
     private int mvpPoint;                   // MVP 추가 포인트
 
     private int winnerPoint;                // 승리팀 획득 포인트
-
-    @Enumerated(EnumType.STRING)
     private WinnerTeam winnerTeam;          // 승리 팀
 }
