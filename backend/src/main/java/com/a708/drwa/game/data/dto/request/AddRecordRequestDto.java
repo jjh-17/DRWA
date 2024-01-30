17534df3a79b41254023e72427baaa18ce15cff9
jjh <jjho311@naver.com>
2024-01-30 10:46:15 +0900

    Fix : @NoArgsConstrucotr 추가

--- backend/src/main/java/com/a708/drwa/game/data/dto/request/AddRecordRequestDto.java
+++ backend/src/main/java/com/a708/drwa/game/data/dto/request/AddRecordRequestDto.java
@@ -1,8 +1,10 @@
 package com.a708.drwa.game.data.dto.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class AddRecordRequestDto {
     private int debateId;
     private int gameId;
