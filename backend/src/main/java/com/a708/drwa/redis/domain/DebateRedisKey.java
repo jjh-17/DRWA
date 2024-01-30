3040835d83bfb072bfa3e47c468d22511c586f29
lkt9899 <lkt9899@naver.com>
2024-01-30 17:45:37 +0900

    Feat : redis Key 추가 및 수정 - roomInfo 추가 - RoomInfo에 속하는 Key들 삭제

--- backend/src/main/java/com/a708/drwa/redis/domain/DebateRedisKey.java
+++ backend/src/main/java/com/a708/drwa/redis/domain/DebateRedisKey.java
@@ -1,43 +1,18 @@
 package com.a708.drwa.redis.domain;
 
 public enum DebateRedisKey {
-    // 방 제목
-    TITLE(":title"),
-    // 공개 여부
-    IS_PRIVATE(":isPrivate"),
-    // 비밀번호
-    PASSWORD(":password"),
-    // 참여자 인원 제한
-    PLAYER_NUM(":playerNum"),
-    // 총 인원 수
-    TOTAL_NUM(":totalNum"),
-    // 발언 시간
-    OPINION_TIME(":opinionTime"),
-    // 준비 시간
-    READY_TIME(":readyTime"),
-    // 질의 시간
-    QUESTION_TIME(":questionTime"),
-    // A팀 유저 목록
-    TEAM_A_LIST(":teamAList"),
-    // B 팀 유저 목록
-    TEAM_B_LIST(":teamBList"),
-    // 배심원 리스트
-    JUROR_LIST(":jurorList"),
-    // 관전자 리스트
-    VIEWER_LIST(":viewerList"),
-    // 제시어
-    LEFT_KEYWORD(":leftKeyword"),
-    RIGHT_KEYWORD(":rightKeyword"),
+    // 방 설정
+    ROOM_INFO("roomInfo"),
     // 페이즈
-    PHASE(":phase"),
+    PHASE("phase"),
     // 현재 발언자
-    CURRENT_SPEAKER(":currentSpeaker"),
+    CURRENT_SPEAKER("currentSpeaker"),
     // MVP
-    MVP(":mvp"),
+    MVP("mvp"),
     // A팀 투표율
-    VOTE_TEAM_A(":voteTeamA"),
+    VOTE_TEAM_A("voteTeamA"),
     // B팀 투표율
-    VOTE_TEAM_B(":voteTeamB"),
+    VOTE_TEAM_B("voteTeamB"),
     // 시작 시간
     START_TIME("startTime"),
     ;
