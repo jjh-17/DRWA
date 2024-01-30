26b4a0b7e93320ce552172e308d6db4ae7214d43
minwoo-kwak <minwoo.kwak540@gmail.com>
2024-01-30 10:08:01 +0900

    Refactor : Social Dto에서 GoogleDto로 이름 변경 및 상속 처리

---
+++ backend/src/main/java/com/a708/drwa/member/dto/GoogleUserInfoResponse.java
@@ -0,0 +1,24 @@
+package com.a708.drwa.member.dto;
+
+import com.a708.drwa.member.type.SocialType;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+// 알 수 없는 JSON 필드 무시
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GoogleUserInfoResponse extends SocialUserInfoResponse{
+    private String id;
+    private String email;
+    private String name;
+    private String picture;
+    private SocialType socialType;
+
+
+}
