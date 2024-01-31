e30db860afc6491c41bef58d91c1885beb787b4c
minwoo-kwak <minwoo.kwak540@gmail.com>
2024-01-31 11:11:14 +0900

    Refactor : slient refresh 과정 단순화 및 정리 Controller에서는 통신만 처리할 수 있도록 나머지 비즈니스 과정은 Service에서 처리할 수 있도록 하였고, 에러 처리를 명확히 해주었습니다.

--- backend/src/main/java/com/a708/drwa/member/controller/AuthController.java
+++ backend/src/main/java/com/a708/drwa/member/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.a708.drwa.member.controller;
 
 import com.a708.drwa.member.dto.AuthDto;
+import com.a708.drwa.member.exception.TokenValidationException;
 import com.a708.drwa.member.service.AuthService;
 import com.a708.drwa.member.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,55 +19,30 @@ import java.util.Map;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    @Value("${jwt.accesstoken.expiretime}")
-    private int accessTokenExpireTime;
-
-    @Value("${jwt.refreshtoken.expiretime}")
-    private int refreshTokenExpireTime;
-
-    private final JWTUtil jwtUtil;
     private final AuthService authService;
 
-    public AuthController(JWTUtil jwtUtil, AuthService authService) {
-        this.jwtUtil = jwtUtil;
+    public AuthController(AuthService authService) {
         this.authService = authService;
     }
 
     /**
-     * Refresh Token을 이용하여 Access Token을 재발급한다.
-     * @param userId : 사용자 아이디
-     * @return 재발급된 액세스 토큰
-     * @throws UnsupportedEncodingException : 인코딩 예외
+     * 리프레시 토큰을 받아서 유효성 검증 후, 새로운 액세스 토큰을 발급한다.
+     * @param userId 사용자 아이디
+     * @return 새로 발급한 액세스 토큰
      */
     @PostMapping("/silent-refresh")
-    public ResponseEntity<Map<String, Object>> silentRefresh(@RequestBody String userId) throws UnsupportedEncodingException {
-        // AuthService를 통해 Redis에서 리프레시 토큰 조회
-        AuthDto authDto = authService.getRefreshToken(userId);
-
-        // 리프레시 토큰이 없으면 401 에러
-        if (authDto == null || authDto.getRefreshToken() == null) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<?> silentRefresh(@RequestBody String userId) {
+        try {
+            // silentRefreshProcess() 메소드를 통해 새로운 액세스 토큰을 발급받는다.
+            String newAccessToken  = authService.silentRefreshProcess(userId);
+            // 새로 발급한 액세스 토큰을 AuthDto 객체에 담아서 반환한다.
+            AuthDto authDto = new AuthDto(userId, newAccessToken);
+            return ResponseEntity.ok(authDto);
+        } catch (TokenValidationException e) { // 토큰 검증 실패 시
+            return ResponseEntity.status(e.getHttpStatus()).body(e.getMessage());
+        } catch (Exception e) { // 기타 에러 발생 시
+            return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        Map<String, Object> result = new HashMap<String, Object>();
-
-        // 리프레시 토큰이 만료되었으면 403 에러
-        if(!jwtUtil.vaildCheck(authDto.getRefreshToken())) {
-            result.put("message", "로그인이 필요합니다");
-            return new ResponseEntity<Map<String, Object>>(result, HttpStatus.FORBIDDEN);
-        }
-
-        // 유효한 경우에는 refreshToken과 accessToken을 재발급.
-        String newAccessToken = jwtUtil.createAccessToken(userId);
-        String newRefreshToken = jwtUtil.createRefreshToken(userId);
-
-        // AuthService를 통해 Redis에 저장된 리프레시 토큰 갱신
-        authService.updateRefreshToken(new AuthDto(userId, newRefreshToken), refreshTokenExpireTime);
-
-        // 새로 발급된 엑세스 토큰 반환
-        result.put("accessToken", newAccessToken);
-
-        return new ResponseEntity<Map<String, Object>>(result, HttpStatus.CREATED);
     }
 
 
--- backend/src/main/java/com/a708/drwa/member/service/AuthService.java
+++ backend/src/main/java/com/a708/drwa/member/service/AuthService.java
@@ -1,7 +1,10 @@
 package com.a708.drwa.member.service;
 
-import com.a708.drwa.member.dto.AuthDto;
+import com.a708.drwa.member.exception.TokenValidationException;
+import com.a708.drwa.member.util.JWTUtil;
 import com.a708.drwa.redis.util.RedisUtil;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,41 +12,73 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class AuthService {
+    @Value("${jwt.accesstoken.expiretime}")
+    private int accessTokenExpireTime;
+
+    @Value("${jwt.refreshtoken.expiretime}")
+    private int refreshTokenExpireTime;
 
     private final RedisUtil redisUtil;
+    private final JWTUtil jwtUtil;
 
-    // RedisUtil을 주입받는다.
-    public AuthService(RedisUtil redisUtil) {
+    // 생성자를 통해 의존성 주입
+    public AuthService(RedisUtil redisUtil, JWTUtil jwtUtil) {
         this.redisUtil = redisUtil;
+        this.jwtUtil = jwtUtil;
     }
 
     /**
-     * 리프레시 토큰을 저장한다.
-     * @param authDto : 리프레시 토큰 정보
-     * @param duration : 저장 기간
+     * 리프레시 토큰을 받아서 유효성 검증 후, 새로운 액세스 토큰을 발급한다.
+     * @param userId 사용자 아이디
+     * @return 새로 발급한 액세스 토큰
      */
-    public void setRefreshToken(AuthDto authDto, long duration) {
+    public String silentRefreshProcess(String userId) {
+        // 사용자 아이디를 키로 리프레시 토큰을 조회한다.
+        String refreshToken = getRefreshToken(userId);
+
+        // 리프레시 토큰이 없는 경우
+        if (refreshToken == null) {
+            // 401 Unauthorized 에러를 발생시킨다.
+            throw new TokenValidationException("Refresh token not found", HttpStatus.UNAUTHORIZED);
+        }
+
+        // 리프레시 토큰이 유효한지 검사한다.
+        if (!jwtUtil.validCheck(refreshToken)) {
+            // 리프레시 토큰이 유효하지 않으면 403 Forbidden 에러를 발생시킨다.
+            throw new TokenValidationException("Invalid refresh token", HttpStatus.FORBIDDEN);
+        }
+
+        // 새로운 액세스 토큰을 발급한다.
+        String newAccessToken = jwtUtil.createAccessToken(userId);
+
+        // 새로운 리프레시 토큰을 발급한다.
+        String newRefreshToken = jwtUtil.createRefreshToken(userId);
+        // 발급한 리프레시 토큰을 저장한다.
+        updateRefreshToken(userId, newRefreshToken, refreshTokenExpireTime);
+
+        // 새로 발급한 액세스 토큰을 반환한다.
+        return newAccessToken;
+    }
+
+    /**
+     * 리프레시 토큰을 Redis에 저장 및 갱신한다.
+     * @param userId 사용자 아이디
+     * @param refreshToken 리프레시 토큰
+     * @param duration 저장 기간
+     */
+    public void updateRefreshToken(String userId, String refreshToken, long duration) {
         // 사용자 아이디를 키로 리프레시 토큰을 저장한다.
         // setData(키, 값, 저장 기간)
-        redisUtil.setData(authDto.getUserId(), authDto.getRefreshToken(), duration);
+        redisUtil.setData(userId, refreshToken, duration);
     }
 
     /**
-     * 리프레시 토큰을 조회한다.
-     * @param userId : 사용자 아이디
-     * @return : 리프레시 토큰 정보
+     * 리프레시 토큰을 Redis에서 조회한다.
+     * @param userId 사용자 아이디
+     * @return 리프레시 토큰
      */
-    public AuthDto getRefreshToken(String userId) {
-        return new AuthDto(userId, redisUtil.getData(userId));
+    public String getRefreshToken(String userId) {
+        return redisUtil.getData(userId);
     }
 
-    /**
-     * 리프레시 토큰을 갱신한다.
-     * @param authDto : 리프레시 토큰 정보
-     * @param duration : 저장 기간
-     */
-    public void updateRefreshToken(AuthDto authDto, long duration) {
-        // setData(키, 값, 저장 기간)
-        redisUtil.setData(authDto.getUserId(), authDto.getRefreshToken(), duration);
-    }
 }
