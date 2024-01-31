91361d74e0fc4ba76996bf406e4bdd550bc40e7b
noowadc <cha12608@naver.com>
2024-01-31 15:37:11 +0900

    Comment : 'RoomRedisRepository' 설명 주석 추가

--- backend/src/main/java/com/a708/drwa/room/repository/RoomRedisRepository.java
+++ backend/src/main/java/com/a708/drwa/room/repository/RoomRedisRepository.java
@@ -4,8 +4,8 @@ import com.a708.drwa.room.domain.Room;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
-
 public interface RoomRedisRepository extends CrudRepository<Room, String> {
     List<Room> findByTitle(String title);
     List<Room> findByKeyword(String keyword);
 }
+// RoomRedisService에서 RedisTemplate를 사용하여 직접 Redis에 접근하므로 필요 없을 듯 !
