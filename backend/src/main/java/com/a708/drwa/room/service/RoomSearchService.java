859de8eadf2531aa72c137dc66da2ba799abe432
noowadc <cha12608@naver.com>
2024-01-31 15:42:24 +0900

    Feat : nori 분석기로 방 검색하는 메서드에 비교 조건으로 제시어 추가

--- backend/src/main/java/com/a708/drwa/room/service/RoomSearchService.java
+++ backend/src/main/java/com/a708/drwa/room/service/RoomSearchService.java
@@ -8,13 +8,12 @@ import com.a708.drwa.room.domain.Room;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class RoomSearchService {
+public class RoomSearchService { // redis에서 방 검색하는 서비스
     @Autowired
     private ElasticsearchClient elasticsearchClient;
 
@@ -22,13 +21,25 @@ public class RoomSearchService {
     private RedisTemplate<String, Room> redisTemplate;
 
     public List<Room> searchRoomsByNori(String query) {
+        // nori 분석기를 통해 elasticsearch에서 검색어와 일치하는 방 정보 반환
         SearchRequest request = new SearchRequest.Builder()
                 .index("room_index")
                 .query(q -> q
-                        .match(m -> m
-                                .field("title")
-                                .query(query)
-                                .analyzer("nori")
+                        .bool(b -> b
+                                .should(s -> s
+                                        .match(m -> m
+                                                .field("title")
+                                                .query(query)
+                                                .analyzer("nori")
+                                        )
+                                )
+                                .should(s -> s
+                                        .match(m -> m
+                                                .field("keyword")
+                                                .query(query)
+                                                .analyzer("nori")
+                                        )
+                                )
                         )
                 )
                 .build();
@@ -37,7 +48,7 @@ public class RoomSearchService {
             SearchResponse<Room> response = elasticsearchClient.search(request, Room.class);
             return response.hits().hits().stream()
                     .map(hit -> redisTemplate.opsForValue().get(hit.id())) // Redis에서 ID를 키로 사용하여 데이터 조회
-                    .filter(room -> room != null) // null이 아닌 결과만 필터링
+                    .filter(room -> room != null)
                     .collect(Collectors.toList());
         } catch (Exception e) {
             return Collections.emptyList();
