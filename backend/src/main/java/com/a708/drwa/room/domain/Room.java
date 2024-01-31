553b1d2480595f6fc365f9d6db93cdcaa76eb1fa
noowadc <cha12608@naver.com>
2024-01-31 15:36:30 +0900

    Comment : 'Room' 설명 주석 추가

--- backend/src/main/java/com/a708/drwa/room/domain/Room.java
+++ backend/src/main/java/com/a708/drwa/room/domain/Room.java
@@ -4,18 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.elasticsearch.annotations.Document;
 import jakarta.persistence.Id;
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
-import org.springframework.data.elasticsearch.annotations.Setting;
-
 
 @Setter
 @Getter
 @Document(indexName = "room_index")
-public class Room {
+public class Room { // Elasticsearch에 저장되는 방 정보
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
