4cfb29140c75f58ac139f3711202411af1b8a1e8
jjh <jjho311@naver.com>
2024-01-31 15:17:25 +0900

    Rename : 위치 변경

--- backend/src/main/java/com/a708/drwa/game/service/PrivateGameRedisResponseDto.java
+++ backend/src/main/java/com/a708/drwa/game/service/PrivateGameRedisResponseDto.java
@@ -2,13 +2,14 @@ package com.a708.drwa.game.service;
 
 import lombok.Builder;
 import lombok.Getter;
+
 import java.util.List;
 import java.util.Map;
 
 // 전적 저장을 위해 Redis에서 받아올 정보
 @Builder
 @Getter
-public class PublicGameRedisResponseDto {
+public class PrivateGameRedisResponseDto {
     private List<Object> teamAList;         // A팀 리스트
     private List<Object> teamBList;         // B팀 리스트
     private List<Object> jurorList;         // 배심원 리스트
