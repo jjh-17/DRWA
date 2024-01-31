cac6ce819542bcc24b44c02a466261d7576d3336
jjh <jjho311@naver.com>
2024-01-31 15:15:13 +0900

    Fix : SqlException 수정, try-catch 추가 - Result, Team의 ordinal을 저장하도록 변경 - try-catch로 전적 저장 실패 throw

--- backend/src/main/java/com/a708/drwa/game/repository/RecordBulkRepository.java
+++ backend/src/main/java/com/a708/drwa/game/repository/RecordBulkRepository.java
@@ -1,6 +1,8 @@
 package com.a708.drwa.game.repository;
 
 import com.a708.drwa.game.domain.Record;
+import com.a708.drwa.game.exception.RecordErrorCode;
+import com.a708.drwa.game.exception.RecordException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,27 +19,32 @@ public class RecordBulkRepository {
     private final JdbcTemplate recordJdbcTemplate;
 
     @Transactional
-    public int[] saveAll(List<Record> records) {
-        String sql
+    public void saveAll(List<Record> records) {
+        final String sql
                 = "INSERT INTO record (member_id, game_id, result, team)\n"
                 + "VALUES (?, ?, ?, ?)";
 
-        return recordJdbcTemplate.batchUpdate(
-                sql,
-                new BatchPreparedStatementSetter() {
-                    @Override
-                    public void setValues(PreparedStatement ps, int i) throws SQLException {
-                        Record record = records.get(i);
-                        ps.setInt(1, record.getMember().getId());
-                        ps.setInt(2, record.getGameInfo().getGameId());
-                        ps.setInt(3, record.getResult().ordinal());
-                        ps.setInt(4, record.getTeam().ordinal());
-                    }
+        try {
+            recordJdbcTemplate.batchUpdate(
+                    sql,
+                    new BatchPreparedStatementSetter() {
+                        @Override
+                        public void setValues(PreparedStatement ps, int i) throws SQLException {
+                            Record record = records.get(i);
+                            ps.setInt(1, record.getMember().getId());
+                            ps.setInt(2, record.getGameInfo().getGameId());
+                            ps.setInt(3, record.getResult().ordinal());
+                            ps.setInt(4, record.getTeam().ordinal());
+                        }
 
-                    @Override
-                    public int getBatchSize() {
-                        return records.size();
-                    }
-                });
+                        @Override
+                        public int getBatchSize() {
+                            return records.size();
+                        }
+                    });
+        } catch(Exception e) {
+            throw new RecordException(RecordErrorCode.RECORD_CREATE_FAIL);
+        }
+
     }
 }
