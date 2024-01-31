9292291ee04a90b28ab287151f973ed4b1f79df8
lkt9899 <lkt9899@naver.com>
2024-01-31 10:36:06 +0900

    Chore : Redis Config 수정

--- backend/src/main/java/com/a708/drwa/redis/config/RedisConfig.java
+++ backend/src/main/java/com/a708/drwa/redis/config/RedisConfig.java
@@ -11,15 +11,14 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
-    @Value("${spring.data.redis.session.host}")
+    @Value("${spring.data.redis.host}")
     private String host;
 
-    @Value("${spring.data.redis.session.port}")
+    @Value("${spring.data.redis.port}")
     private int port;
 
     @Value("${spring.data.redis.password}")
@@ -40,11 +39,11 @@ public class RedisConfig {
         redisTemplate.setConnectionFactory(redisConnectionFactory());
 
         // 일반적인 key:value의 경우 시리얼라이저
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setKeySerializer(new GenericJackson2JsonRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         // Hash를 사용할 경우 시리얼라이저
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new GenericJackson2JsonRedisSerializer());
         redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         // 모든 경우
