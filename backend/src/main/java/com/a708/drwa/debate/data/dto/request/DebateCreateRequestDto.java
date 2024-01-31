8095f36186bf447bb0c02b148a6bc04b786de181
lkt9899 <lkt9899@naver.com>
2024-01-31 16:50:23 +0900

    Refactor : DebateCategory Enum Type 으로 변경

--- backend/src/main/java/com/a708/drwa/debate/data/dto/request/DebateCreateRequestDto.java
+++ backend/src/main/java/com/a708/drwa/debate/data/dto/request/DebateCreateRequestDto.java
@@ -1,11 +1,11 @@
 package com.a708.drwa.debate.data.dto.request;
 
-import com.a708.drwa.debate.domain.DebateCategory;
+import com.a708.drwa.debate.enums.DebateCategory;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Data
 public class DebateCreateRequestDto {
-    private int debateCategoryId;
+    private DebateCategory debateCategory;
 }
