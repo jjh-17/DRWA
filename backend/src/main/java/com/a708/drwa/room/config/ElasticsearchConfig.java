df1110c0f238f7c9c93ac8437e7ef753763ec22e
noowadc <cha12608@naver.com>
2024-01-31 15:36:08 +0900

    Comment : ElasticsearchConfig 설명 주석 추가

--- backend/src/main/java/com/a708/drwa/room/config/ElasticsearchConfig.java
+++ backend/src/main/java/com/a708/drwa/room/config/ElasticsearchConfig.java
@@ -13,17 +13,17 @@ import org.springframework.context.annotation.Configuration;
 public class ElasticsearchConfig {
 
     @Bean
-    public ElasticsearchClient elasticsearchClient() {
+    public ElasticsearchClient elasticsearchClient() { // Elasticsearch 서버와의 통신
         // ElasticsearchTransport를 사용하여 ElasticsearchClient 생성
         ElasticsearchTransport transport = new RestClientTransport(
-                RestClient.builder(new HttpHost("localhost", 9200)).build(), // RestClient.builder().build()를 사용하여 RestClient 인스턴스 생성
+                RestClient.builder(new HttpHost("localhost", 9200)).build(),
                 new JacksonJsonpMapper()
         );
         return new ElasticsearchClient(transport);
     }
 
     @Bean
-    public boolean createIndex(ElasticsearchClient elasticsearchClient) {
+    public boolean createIndex(ElasticsearchClient elasticsearchClient) { // 인덱스 생성 (+ 한국어 텍스트 분석을 위한 매핑)
         try {
             boolean exists = elasticsearchClient.indices().exists(e -> e.index("room_index")).value();
             if (!exists) {
