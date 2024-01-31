7e39e7eedb80511d9d298d3ed811a6ce8a83130a
lkt9899 <lkt9899@naver.com>
2024-01-31 11:03:14 +0900

    Style : package 이동

--- backend/src/main/java/com/a708/drwa/debate/data/dto/DebateMemberDto.java
+++ backend/src/main/java/com/a708/drwa/debate/data/dto/DebateMemberDto.java
@@ -1,6 +1,6 @@
 package com.a708.drwa.debate.data.dto;
 
-import com.a708.drwa.debate.domain.enums.Role;
+import com.a708.drwa.debate.enums.Role;
 import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
