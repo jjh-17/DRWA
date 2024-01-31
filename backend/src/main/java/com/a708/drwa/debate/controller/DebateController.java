08f439b458a09e9cbc1e436ce237edc807df39b1
lkt9899 <lkt9899@naver.com>
2024-01-31 10:03:23 +0900

    Feat : Debate Start 기능 추가

--- backend/src/main/java/com/a708/drwa/debate/controller/DebateController.java
+++ backend/src/main/java/com/a708/drwa/debate/controller/DebateController.java
@@ -2,6 +2,7 @@ package com.a708.drwa.debate.controller;
 
 import com.a708.drwa.debate.data.dto.request.DebateCreateRequestDto;
 import com.a708.drwa.debate.data.dto.request.DebateJoinRequestDto;
+import com.a708.drwa.debate.data.dto.request.DebateStartRequestDto;
 import com.a708.drwa.debate.service.DebateService;
 import com.a708.drwa.debate.service.OpenViduService;
 import com.a708.drwa.redis.util.RedisKeyUtil;
@@ -68,4 +69,9 @@ public class DebateController {
         }
     }
 
+    @PostMapping("/start")
+    public ResponseEntity<Void> start(@RequestBody DebateStartRequestDto debateStartRequestDto) {
+        debateService.start(debateStartRequestDto);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
 }
