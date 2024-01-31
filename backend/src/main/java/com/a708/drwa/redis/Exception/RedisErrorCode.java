f9246e8e76e11ed96abb419187b031fa2f2723af
jjh <jjho311@naver.com>
2024-01-31 15:32:58 +0900

    Feat : Redis 관련 에러 코드 추가

--- backend/src/main/java/com/a708/drwa/redis/Exception/RedisErrorCode.java
+++ backend/src/main/java/com/a708/drwa/redis/Exception/RedisErrorCode.java
@@ -1,4 +1,4 @@
-package com.a708.drwa.redis.Exception;
+package com.a708.drwa.redis.exception;
 
 import com.a708.drwa.global.exception.ErrorCode;
 import lombok.Getter;
@@ -8,9 +8,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum RedisErrorCode implements ErrorCode {
 
-    REDIS_UPDATE_FAIL(500, "Redis Craete Fail", "Redis 데이터 자장/수정에 실패하였습니다."),
-    REDIS_READ_FAIL(500, "Redis Read Fail", "Redis에서 데이터 읽기에 실패하였습니다."),
-    REDIS_DELETE_FAIL(500, "Redis Delete Fail", "Redis에서 데이터 삭제에 실패하였습니다.");
+    REDIS_UPDATE_FAIL(500, "REDIS_01", "Redis 데이터 저장/수정에 실패하였습니다."),
+    REDIS_READ_FAIL(500, "REDIS_02", "Redis에서 데이터 읽기에 실패하였습니다."),
+    REDIS_DELETE_FAIL(500, "REDIS_03", "Redis에서 데이터 삭제에 실패하였습니다."),
+    REDIS_SERVER_ERROR(500, "REDIS_04", "Redis 서버 에러"),
+    ;
 
     private final int statusCode;
     private final String errorCode;
