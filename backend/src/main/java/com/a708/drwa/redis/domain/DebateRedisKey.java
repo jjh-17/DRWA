7ff207663a89ecdbf5540b6212af486bd927a553
lkt9899 <lkt9899@naver.com>
2024-01-31 10:12:48 +0900

    Feat : Redis Key 수정

--- backend/src/main/java/com/a708/drwa/redis/domain/DebateRedisKey.java
+++ backend/src/main/java/com/a708/drwa/redis/domain/DebateRedisKey.java
@@ -3,6 +3,14 @@ package com.a708.drwa.redis.domain;
 public enum DebateRedisKey {
     // 방 설정
     ROOM_INFO("roomInfo"),
+    // A팀 유저 목록
+    TEAM_A_LIST(":teamAList"),
+    // B 팀 유저 목록
+    TEAM_B_LIST(":teamBList"),
+    // 배심원 리스트
+    JUROR_LIST(":jurorList"),
+    // 관전자 리스트
+    VIEWER_LIST(":viewerList"),
     // 페이즈
     PHASE("phase"),
     // 현재 발언자
@@ -15,6 +23,8 @@ public enum DebateRedisKey {
     VOTE_TEAM_B("voteTeamB"),
     // 시작 시간
     START_TIME("startTime"),
+    // 제시어
+    KEY_WORD(":keyWord"),
     ;
 
     private final String string;
