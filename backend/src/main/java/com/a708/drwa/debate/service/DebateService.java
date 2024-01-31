deb38f2098c4ae258d4b0dad57a941bfa5b2b05a
lkt9899 <lkt9899@naver.com>
2024-01-31 09:58:45 +0900

    Feat : Debate Create 추가

--- backend/src/main/java/com/a708/drwa/debate/service/DebateService.java
+++ backend/src/main/java/com/a708/drwa/debate/service/DebateService.java
@@ -2,8 +2,11 @@ package com.a708.drwa.debate.service;
 
 import com.a708.drwa.debate.data.dto.request.DebateCreateRequestDto;
 import com.a708.drwa.debate.data.dto.request.DebateJoinRequestDto;
+import com.a708.drwa.debate.domain.Debate;
+import com.a708.drwa.debate.domain.DebateCategory;
 import com.a708.drwa.debate.exception.DebateErrorCode;
 import com.a708.drwa.debate.exception.DebateException;
+import com.a708.drwa.debate.repository.DebateCategoryRepository;
 import com.a708.drwa.debate.repository.DebateRepository;
 import io.openvidu.java.client.OpenVidu;
 import io.openvidu.java.client.OpenViduHttpException;
@@ -18,15 +21,23 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class DebateService {
     private final DebateRepository debateRepository;
+    private final DebateCategoryRepository debateCategoryRepository;
 
+    /**
+     * 토론 방 생성
+     * @param debateCreateRequestDto
+     * @return
+     */
     @Transactional
     public int create(DebateCreateRequestDto debateCreateRequestDto) {
-        return debateRepository.save(debateCreateRequestDto.toEntity()).getDebateId();
-    }
+        // 카테고리 가져옴
+        DebateCategory debateCategory = debateCategoryRepository
+                .findById(debateCreateRequestDto.getDebateCategoryId())
+                .orElse(null);
 
-//    public String join(DebateJoinRequestDto debateJoinRequestDto) {
-//
-//    }
+        return debateRepository.save(Debate.builder().debateCategory(debateCategory).build())
+                .getDebateId();
+    }
 
     public Boolean isExistDebate(DebateJoinRequestDto debateJoinRequestDto) {
         // 존재하지 않는 방일 경우
