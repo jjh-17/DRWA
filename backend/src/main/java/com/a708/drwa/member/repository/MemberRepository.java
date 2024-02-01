b02964231dc79ad8f8ac5063e87d6ea6700418a6
jjh <jjho311@naver.com>
2024-02-01 13:53:41 +0900

    Feat : 멤버 ID 리스트를 받아 검색을 수행하는 메서드 추가

--- backend/src/main/java/com/a708/drwa/member/repository/MemberRepository.java
+++ backend/src/main/java/com/a708/drwa/member/repository/MemberRepository.java
@@ -2,13 +2,14 @@ package com.a708.drwa.member.repository;
 
 import com.a708.drwa.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
 
     Optional<Member> findByUserId(String userId);
 
-
     void deleteByUserId(String userId);
+
+    Optional<List<Member>> findAllByIdIn(List<Integer> ids);
 }
