5de3118b983e44f065f0380e18ad396000f6cdbe
minwoo-kwak <minwoo.kwak540@gmail.com>
2024-02-01 13:40:09 +0900

    Fix : eslint 충돌

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -22,20 +22,20 @@ module.exports = {
   },
   // 사용자 편의 규칙 추가
   rules: {
-    'prettier/prettier': [
-      'error',
-      // 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
-      // https://prettier.io/docs/en/options.html
-      {
-        singleQuote: true,
-        semi: false,
-        tabWidth: 2,
-        trailingComma: 'none',
-        printWidth: 100,
-        bracketSpacing: true,
-        arrowParens: 'avoid',
-        endOfLine: 'auto'
-      }
-    ]
+    // 'prettier/prettier': [
+    //   'error',
+    //   // 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
+    //   // https://prettier.io/docs/en/options.html
+    //   {
+    //     singleQuote: true,
+    //     semi: false,
+    //     tabWidth: 2,
+    //     trailingComma: 'none',
+    //     printWidth: 100,
+    //     bracketSpacing: true,
+    //     arrowParens: 'avoid',
+    //     endOfLine: 'auto'
+    //   }
+    // ]
   }
 }
