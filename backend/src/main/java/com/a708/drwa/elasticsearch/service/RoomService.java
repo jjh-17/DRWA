90111f07445b3b11dac85dfa494aeeb5cc1ed76e
noowadc <cha12608@naver.com>
2024-02-01 10:27:35 +0900

    Feat : Redis에서 방 조회 기능

--- backend/src/main/java/com/a708/drwa/elasticsearch/service/RoomService.java
+++ backend/src/main/java/com/a708/drwa/elasticsearch/service/RoomService.java
@@ -1,23 +1,30 @@
-package com.a708.drwa.elasticsearch.service;
+package com.a708.drwa.room.service;
 
-import com.a708.drwa.elasticsearch.domain.Room;
-import com.a708.drwa.elasticsearch.repository.RoomRepository;
+import com.a708.drwa.room.domain.Room;
+import com.a708.drwa.room.repository.RoomRedisRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class RoomService {
+public class RoomRedisService {
+
     @Autowired
-    private RoomRepository roomRepository;
+    private RedisTemplate<String, Room> redisTemplate;
 
-    public Room getRoomByTitle(String title){//제목 검색해서 검색어에 해당하는 방 가정오기
-        Optional<Room> roomOptional = roomRepository.findByTitle(title);
-        return roomOptional.orElse(null);
-    }
 
-    public Room getRoomByKeyword(String keyword){//키워드 검색해서 검색어에 해당하는 방 가져오기
-        Optional<Room> roomOptional = roomRepository.findByKeyword(keyword);
-        return roomOptional.orElse(null);
+    public Room getRoomFromRedis(String roomId) {
+        return redisTemplate.opsForValue().get(roomId);
     }
 }
+//    public Room getRoomByTitle(String title){//제목 검색해서 검색어에 해당하는 방 가정오기
+//        Optional<Room> roomOptional = roomRepository.findByTitle(title);
+//        return roomOptional.orElse(null);
+//    }
+//
+//    public Room getRoomByKeyword(String keyword){//키워드 검색해서 검색어에 해당하는 방 가져오기
+//        Optional<Room> roomOptional = roomRepository.findByKeyword(keyword);
+//        return roomOptional.orElse(null);
+//    }
+//}
