522a8b3f1323b0465a56740b4b5db8a1c75790a6
jjh <jjho311@naver.com>
2024-02-05 17:55:27 +0900

    Feat : memberId 리스트를 받아 검색을 수행하는 메서드 추가

--- backend/src/main/java/com/a708/drwa/profile/repository/ProfileRepository.java
+++ backend/src/main/java/com/a708/drwa/profile/repository/ProfileRepository.java
@@ -3,8 +3,11 @@ package com.a708.drwa.profile.repository;
 import com.a708.drwa.profile.domain.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProfileRepository extends JpaRepository<Profile, Integer> {
     Optional<Profile> findByMemberId(Integer memberId);
+
+    Optional<List<Profile>> findAllByMemberId(List<Integer> memberIds);
 }
