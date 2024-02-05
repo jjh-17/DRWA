67e381aef80d884de417180020510bcdc2d6783f
jjh <jjho311@naver.com>
2024-02-05 17:52:55 +0900

    Feat : 다른 Enum 클래스 형식에 맞추어 수정

--- backend/src/main/java/com/a708/drwa/game/data/dto/response/WinnerTeam.java
+++ backend/src/main/java/com/a708/drwa/game/data/dto/response/WinnerTeam.java
@@ -2,5 +2,17 @@ package com.a708.drwa.game.data.dto.response;
 
 // 승리한 팀
 public enum WinnerTeam {
-    A, B, TIE
+    A("teamA"),
+    B("teamB"),
+    TIE("tie");
+
+    private final String string;
+
+    WinnerTeam(String string) {
+        this.string = string;
+    }
+
+    public String string() {
+        return string;
+    }
 }
