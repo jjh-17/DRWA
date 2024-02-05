4d819fb785703419cf6b386937c8252506cb1513
noowadc <cha12608@naver.com>
2024-02-05 16:57:36 +0900

    Refactor : Elasticsearch 연결 정보를 외부 설정에서 가져오도록 수정

--- backend/src/main/java/com/a708/drwa/room/config/ElasticsearchConfig.java
+++ backend/src/main/java/com/a708/drwa/room/config/ElasticsearchConfig.java
@@ -6,16 +6,24 @@ import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.elasticsearch.client.RestClient;
 import org.apache.http.HttpHost;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticsearchConfig {
 
+    @Value("${spring.data.elasticsearch.client.rest.uris}")
+    private String url;
+
+    @Value("${spring.data.elasticsearch.client.rest.port}")
+    private int port;
     @Bean
     public ElasticsearchClient elasticsearchClient() {
-        RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200)).build();
-        ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+        ElasticsearchTransport transport = new RestClientTransport(
+                RestClient.builder(new HttpHost(url, port)).build(),
+                new JacksonJsonpMapper()
+        );
         return new ElasticsearchClient(transport);
     }
 
