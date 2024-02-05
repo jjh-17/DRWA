282d7659f44134b09fac441da9c43c4e7f99d709
noowadc <cha12608@naver.com>
2024-02-05 17:00:02 +0900

    Chore : RoomController의 의존성 주입 방식 변경

--- backend/src/main/java/com/a708/drwa/room/controller/RoomController.java
+++ backend/src/main/java/com/a708/drwa/room/controller/RoomController.java
@@ -2,23 +2,21 @@ package com.a708.drwa.room.controller;
 
 import com.a708.drwa.room.domain.Room;
 import com.a708.drwa.room.service.RoomSearchService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/room")
+@RequiredArgsConstructor
 public class RoomController {
-
     private final RoomSearchService roomSearchService;
-
-    @Autowired
-    public RoomController(RoomSearchService roomSearchService) {
-        this.roomSearchService = roomSearchService;
-    }
 
     @GetMapping("/search/title")
     public ResponseEntity<List<Room>> RoomsByTitle(@RequestParam String query) {
@@ -31,5 +29,4 @@ public class RoomController {
         List<Room> rooms = roomSearchService.searchRoomsByKeyword(query);
         return ResponseEntity.ok(rooms);
     }
-
 }
