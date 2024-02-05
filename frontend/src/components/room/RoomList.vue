6ecfabec85d3bae2a1764a0b6aa1f9f6ed3c62e9
KimJihwan98 <wlghks98k@gmail.com>
2024-02-05 16:53:25 +0900

    Feat : 방목록 피니아 추가

--- frontend/src/components/room/RoomList.vue
+++ frontend/src/components/room/RoomList.vue
@@ -1,15 +1,30 @@
 <template>
-    <div roomList_container>
-
+    <div class="roomList-container">
+        hi
+      <!-- <h2>Rooms in Category: {{ categoryName }}</h2> -->
+      <ul>
+        <!-- <li v-for="room in roomStore.rooms" :key="room.id">
+          {{ room.name }} - {{ room.description }}
+        </li> -->
+      </ul>
     </div>
-</template>
+  </template>
 
-<script>
-    export default {
-        
-    }
+<script setup>
+import { onMounted } from 'vue'
+import { useRoomStore } from '@/components/stores/roomStore'
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  selectedCategory: String
+})
+
+const roomStore = useRoomStore()
+const categoryName = 'props.activeCategory'
+
+onMounted(() => {
+  roomStore.fetchRooms(categoryName)
+})
 </script>
 
-<style>
-
-</style>
+<style></style>
