eef78cbe746d005cb00c7886e24fc513d7cb550e
minwoo-kwak <minwoo.kwak540@gmail.com>
2024-02-05 17:24:56 +0900

    Fix : SearchResult import 위치 변경

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainView from '../views/MainView.vue'
-import SearchResults from '@/components/search/SearchResults.vue'
+import SearchResults from '@/views/SearchResults.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
