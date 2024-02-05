40775862b3fd7801c0f08e4a1df0cef61fa5eefe
noowadc <cha12608@naver.com>
2024-02-05 16:12:32 +0900

    Refactor : Redis에 결과를 캐싱을 위한 searchRooms 개선

--- backend/src/main/java/com/a708/drwa/room/config/ElasticsearchConfig.java
+++ backend/src/main/java/com/a708/drwa/room/config/ElasticsearchConfig.java
@@ -13,28 +13,25 @@ import org.springframework.context.annotation.Configuration;
 public class ElasticsearchConfig {
 
     @Bean
-    public ElasticsearchClient elasticsearchClient() { // Elasticsearch 서버와의 통신
-        ElasticsearchTransport transport = new RestClientTransport(
-                RestClient.builder(new HttpHost("localhost", 9200)).build(),
-                new JacksonJsonpMapper()
-        );
+    public ElasticsearchClient elasticsearchClient() {
+        RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200)).build();
+        ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
         return new ElasticsearchClient(transport);
     }
 
     @Bean
-    public boolean createIndex(ElasticsearchClient elasticsearchClient) { // 인덱스 생성 (+ 한국어 텍스트 분석을 위한 매핑)
+    public boolean createIndex(ElasticsearchClient elasticsearchClient) {
         try {
             boolean exists = elasticsearchClient.indices().exists(e -> e.index("room_index")).value();
-            if (!exists) { // 인덱스 없으면
-                // 인덱스 생성 요청
+            if (!exists) {
                 var response = elasticsearchClient.indices().create(c -> c
                         .index("room_index")
                         .mappings(m -> m
                                 .properties("title", p -> p
-                                        .text(t -> t.analyzer("korean"))
+                                        .text(t -> t.analyzer("nori"))
                                 )
                                 .properties("keyword", p -> p
-                                        .text(t -> t.analyzer("korean"))
+                                        .text(t -> t.analyzer("nori"))
                                 )
                         )
                 );
