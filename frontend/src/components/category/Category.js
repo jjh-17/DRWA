dd559113623086cd09126d8d99cfa9c02ac4eed8
KimJihwan98 <wlghks98k@gmail.com>
2024-02-07 17:57:37 +0900

    Refactor : Category 목록 수정

--- frontend/src/components/category/Category.js
+++ frontend/src/components/category/Category.js
@@ -2,7 +2,7 @@ export const categories = [
     { name: '전체', english: 'all' },
     { name: '사람', english: 'character' },
     { name: '사랑', english: 'love' },
-    { name: '쇼핑', english: 'shopping' },
+    { name: '음식', english: 'food' },
     { name: '경제', english: 'economy' },
     { name: '스포츠', english: 'sports' },
     { name: '음악', english: 'music' },
