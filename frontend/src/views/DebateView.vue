4ae1a8838a718871c5de34fe725210ba895aa5db
jjh <jjho311@naver.com>
2024-02-11 16:20:02 +0900

    Feat : 토론 관련 메서드 추가 중

--- frontend/src/views/DebateView.vue
+++ frontend/src/views/DebateView.vue
@@ -1,13 +1,16 @@
 <script setup>
 import DebateHeaderBar from '@/components/debate/DebateHeaderBar.vue'
+import DebateVideos from "@/components/debate/DebateVideos.vue";
+import ChattingBar from "@/components/debate/ChattingBar.vue";
 import DebateBottomBar from '@/components/debate/DebateBottomBar.vue'
-
+import { createSession, createToken } from "@/api/debate";
 import { ref, reactive, toRefs, computed, defineProps } from 'vue'
 import { useDebateStore } from '@/stores/useDebateStore'
 import { useAuthStore } from "@/stores/useAuthStore"
 import { useRoute } from 'vue-router'
 import { OpenVidu } from "openvidu-browser";
 import UserVideo from "@/components/debate/UserVideo.vue"
+import { team } from '@/components/common/Team.js'
 
 // === 변수 ===
 // Debate 정보
@@ -20,8 +23,6 @@ const headerBarTitle = ref('[임시]제목입니다.')
 
 // User 정보
 const authStore = useAuthStore();
-const userId = ref('');
-const nickname = ref('');
 
 // Room 정보 => API 호출 필요
 const roomInfo = {
@@ -41,33 +42,60 @@ const roomInfo = {
   thumbnailB : ref(''),           // 썸네일 B
 }
 
+const userInfo = {
+  memberId: ref(1),
+  userId: ref(''),
+  nickname: ref(''),
+  team: ref(''),
+}
+
 // OPEN_VIDU 정보
-const OV = ref(undefined);            // 비디오
-const session = ref(undefined);       // 세션 정보
-const token = ref('')
 const openVidu = {
-  // mainStreamManager : ref(undefined), // 호스트 카메라
-  publisher : ref(undefined),         // 자기 자신
-  subscribersLeft : ref([]),          // 팀 A 카메라
-  subscribersRight : ref([]),         // 팀 B 카메라
-}
-// const mainStreamManagerComputed = computed(() => openVidu.mainStreamManager.value);  // 메인 비디오 Computed
-const publisherComputed = computed(() => openVidu.publisher.value);                  
-const subscribersLeftComputed = computed(() => openVidu.subscribersLeft.value);
-const subscribersRightComputed = computed(() => openVidu.subscribersRight.value);
+  OV : ref(undefined),            // 비디오
+  session: ref(undefined),        // 세션 정보
 
-// 카메라, 오디오 정보
+  sessionId: ref(''),
+  
+  publisher: ref(undefined),      // 자기 자신
+  subscriber: ref(undefined),
+  subscribersLeft : ref([]),      // 팀 A 리스트
+  subscribersRight: ref([]),      // 팀 B 리스트
+  subscribersJuror: ref([]),      // 배심원 리스트
+  subscribersWatcher: ref([]),    // 관전자 리스트
+  publisherComputed: computed(() => openVidu.publisher.value),
+  subscribersLeftComputed: computed(() => openVidu.subscribersLeft.value),
+  subscribersRightComputed : computed(() => openVidu.subscribersRight.value),
+  subscribersJurorComputed : computed(() => openVidu.subscribersJuror.value),
+  subscrbersWatcherComputed : computed(() => openVidu.subscribersWatcher.value), 
+}     
+
+
 const communication = {
-  isAudioOn : ref(false),       // 기본은 음소거
-  isVideoOn : ref(true),        // 기본은 카메라 ON
-  selectedAudio : ref(""),      // 오디오 변경시 사용할 변수
-  selectedVideo : ref(""),      // 카메라 변경시 사용할 변수 
+  // 마이크/카메라 켜짐 여부
+  isMicOn: ref(false),
+  isCameraOn: ref(false),
+  isShareOn: ref(false),
+
+  // 마이크/카메라 핸들링 권한 여부
+  isMicHandleAvailable: ref(true),
+  isCameraHandleAvailable: ref(false),
+  isShareHandleAvailable: ref(false),
+
+  // 현재 사용중인 마이크/카메라 기기 정보
+  selectedMic: ref(""),
+  selectedCamera: ref(""),
+
+  // 가용 가능한 마이크/카메라 정보
+  micList: ref([]),
+  cameraList: ref([]),
+  micListComputed: computed(() => communication.micList.value),
+  cameraListComputed: computed(() => communication.cameraList.value),
 }
 
-// 채팅창 정보
-const inputMessage = ref("")
-const targetTeam = ref("")
+// 채팅방
 const chatting = {
+  targetTeam: ref(team[3].english),
+  inputMessage: ref(''),
   messagesLeft : ref([]),
   messagesRight : ref([]),
   messagesAll : ref([]),
@@ -75,72 +103,69 @@ const chatting = {
 
 // === 세션 관련 메서드 ===
 // 세션 합류 메서드
-function joinSession(team) {
+function joinSession() {
   // openvidu, 세션 객체 생성
-  OV.value = new OpenVidu()
-  session.value = OV.value.initSession();
+  openVidu.OV.value = new OpenVidu()
+  openVidu.session.value = openVidu.OV.value.initSession();
 
   // 세션 시작 작업
-  session.value.on("streamCreated", ({ stream }) => {
+  openVidu.session.value.on("streamCreated", ({ stream }) => {
     // 새로운 subscriber
-    const subscriber = session.value.subscribe(stream)
+    const subscriber = openVidu.session.value.subscribe(stream)
 
-    // team에 따라 다른 subscrber 합류
-    if (team == "teamLeft") {
-      openVidu.subscribersLeft.value.push(subscriber) 
-    } else if(team == "teamRight") {
-      openVidu.subscribersRight.value.push(subscriber)
-    } else {
-      console.log('세션 들어가기 실패');
-    }
+    // 관전자로 편입(host도 일단 관전자로?)
+    openVidu.subscribersWatcher.value.push(subscriber);
+    userInfo.team.value = team[3].english;
+    communication.isMicHandleAvailable.value = false;
+    communication.isCameraHandleAvailable.value = false;
+    communication.isShareHandleAvailable.value = false;
+    // handle
+    
+    // // hostId == memberId면 초기팀 A, 그 외엔 관전자
+    // if (roomInfo.hostId == userInfo.memberId) {
+    //   openVidu.subscribersLeft.value.push(subscriber);
+    // } else {
+    //   openVidu.subscribersWatcher.value.push(subscriber);
+    // }
   })
 
-  // 세션 종료 작업
-  session.value.on("streamDestroyed", ({ stream }) => {
-    let index;
-    if (team == "teamLeft") {
-      index = openVidu.subscribersLeft.value.indexOf(stream.streamManager, 0)
-      if (index >= 0) {
-        openVidu.subscribersLeft.value.splice(index, 1)
-      }
-    } else if (team == "teamRight") {
-      index = openVidu.subscribersRight.value.indexOf(stream.streamManager, 0);
-      if (index >= 0) {
-        openVidu.subscribersRight.value.splice(index, 1)
-      }
-    }
+  // 세션 종료 작업 => 현재 팀에서 나간다.
+  openVidu.session.value.on("streamDestroyed", ({ stream }) => {
+    leaveTeam(stream.streamManager);
   })
 
   // 비동기 관련 오류 처리
-  session.value.on("exception", ({ exception }) => {
+  openVidu.session.value.on("exception", ({ exception }) => {
     console.warn(exception);
   });
 
-  // 메시지 수신 처리
-  session.value.on(`signal:chat`, (event) => {
+  // 이벤트 수신 처리 => targetTeam에 따라 메시지 저장 공간 변화
+  openVidu.session.value.on(`signal:chat`, (event) => {
     const messageData = JSON.parse(event.data);
-    if (targetTeam.value == "teamLeft") {
+
+    if (messageData.targetTeam == team[0].english) {
       chatting.messagesLeft.value.push(messageData)
-    } else if (targetTeam.value == "teamRight") {
+    } else if (messageData.targetTeam == team[1].english) {
       chatting.messagesRight.value.push(messageData)
-    } else if (targetTeam.value == "teamAll") {
+    } else if (messageData.targetTeam == team[2].english
+                || messageData.targetTeam == team[3].english) {
       chatting.messagesAll.value.push(messageData)
     }
   })
 
-  // 5) 토큰이 유효하면 세션 연결
+  // 토큰이 유효하면 세션 연결
   getToken().then((token) => {
-    session.value
-      .connect(token1, { clientData: userId.value })
+    openVidu.session.value
+      .connect(token, { clientData: userInfo.userId.value })
       .then(() => {
         // 사용자의 openVidu 설정 생성
-        let publisher_tmp = OV.value.initPublisher(
+        let publisher_tmp = openVidu.OV.value.initPublisher(
           undefined,
           {
             audioSource: undefined, // 오디오 => 기본 microphone
             videoSource: undefined, // 비디오 => 기본 webcam
-            publishAudio: communication.isAudioOn.value,  // 시작 오디오 상태(ON/OFF)
-            publishVideo: communication.isVideoOn.value,  // 시작 비디오 상태(ON/OFF)
+            publishAudio: false,  // 시작 오디오 상태(ON/OFF)
+            publishVideo: false,  // 시작 비디오 상태(ON/OFF)
             resolution: "640x480",  // 카메라 해상도
             frameRate: 30,          // 카메라 프레임
             insertMode: "APPEND",   // 비디오가 추가되는 방식
@@ -149,7 +174,7 @@ function joinSession(team) {
 
         // 사용자 publisher 설정 수정 및 송출 
         openVidu.publisher.value = publisher_tmp;
-        session.value.publish(publisher_tmp);
+        openVidu.session.value.publish(publisher_tmp);
 
         // 미디어 정보를 불러온다.
         getMedia();
@@ -163,17 +188,65 @@ function joinSession(team) {
   window.addEventListener("beforeunload", leaveSession);
 }
 
+// targetTeam으로 합류
+function joinTeam(targetTeam, subscriber) {
+  // 기존 팀 나감
+  leaveTeam(subscriber);
+
+  // 현재 팀정보 수정
+  userInfo.team.value = targetTeam;
+
+  // team에 따라 다른 subscrber 합류
+  if (userInfo.team.value == team[0].english) {
+    openVidu.subscribersLeft.value.push(subscriber);
+  } else if (userInfo.team.value == team[1].english) {
+    openVidu.subscribersRight.value.push(subscriber);
+  } else if (userInfo.team.value == team[2].english) {
+    openVidu.subscribersJuror.value.push(subscriber);
+  } else if (userInfo.team.value == team[3].english) {
+    openVidu.subscribersWatcher.value.push(subscriber);
+  }
+}
+
+// 현재 팀에서 나가기
+function leaveTeam(team, subscriber) {
+  let index;
+  if (userInfo.team.value == team[0].english) {
+    index = openVidu.subscribersLeft.value.indexOf(subscriber, 0)
+    if (index >= 0) {
+      openVidu.subscribersLeft.value.splice(index, 1)
+    }
+  } else if (userInfo.team.value == team[1].english) {
+    index = openVidu.subscribersRight.value.indexOf(subscriber, 0);
+    if (index >= 0) {
+      openVidu.subscribersRight.value.splice(index, 1)
+    }
+  } else if (userInfo.team.value == team[2].english) {
+    index = openVidu.subscribersLeft.value.indexOf(subscriber, 0)
+    if (index >= 0) {
+      openVidu.subscribersLeft.value.splice(index, 1)
+    }
+  } else if (userInfo.team.value == team[3].english) {
+    index = openVidu.subscribersLeft.value.indexOf(subscriber, 0)
+    if (index >= 0) {
+      openVidu.subscribersLeft.value.splice(index, 1)
+    }
+  }
+}
+
 // 세션 나가기 메서드
 function leaveSession() {
-  // --- 7) Leave the session by calling 'disconnect' method over the Session object ---
-  if (session.value) session.value.disconnect()
+  // 세션 연결 종료
+  if (openVidu.session.value) openVidu.session.value.disconnect()
 
   // Empty all properties...
-  session.value = undefined;
-  openVidu.publisher = undefined;
-  openVidu.subscribersLeft = undefined;
-  openVidu.subscribersRight = undefined;
-  OV.value = undefined;
+  openVidu.session.value = undefined;
+  openVidu.publisher.value = undefined;
+  openVidu.subscribersLeft.value = [];
+  openVidu.subscribersRight.value = [];
+  openVidu.subscribersJuror.value = [];
+  openVidu.subscribersWatcher.value = [];
+  openVidu.OV.value = undefined;
 
   // 윈도우 종료 시 세션 나가기 이벤트 삭제
   window.removeEventListener("beforeunload", leaveSession)
@@ -182,33 +255,26 @@ function leaveSession() {
 // API 호출 메서드를 이용하여 토큰 반환
 async function getToken() {
   // API 호출 필요
-  await createSession(roomInfo.category.value)
+  await createSession(openVidu.sessionId.value)
   const token = await createToken(debateId)
   return token
 }
 
-// 세션 생성 API 호출
-async function createSession(category) {
-  const response = await category;
-}
-
-// 토큰 생성 API 호출
-async function createToken(debateId) {
-  const response = await token.value
-  return response
-}
-
+// === 채팅방 메서드 ===
 // 채팅 보내기 메서드
-function sendMessage(event) {
+function sendMessage(event, inputMessage) {
   event.preventDefault();
-  if (inputMessage.value.trim()) {
-    // messages.value.push({username : myUserName.value, message : inputMessage.value})
+  if (inputMessage.trim()) {
     // 다른 참가원에게 메시지 전송하기
-    session.value.signal({
-      data: JSON.stringify({ username: nickname.value, message: inputMessage.value }), // 메시지 데이터를 문자열로 변환해서 전송
+    openVidu.session.value.signal({
+      // 메시지 데이터를 문자열로 변환해서 전송
+      data: JSON.stringify({
+        username: userInfo.nickname.value,
+        targetTeam: chatting.targetTeam.value,
+        message: inputMessage,
+      }), 
       type: 'chat' // 신호 타입을 'chat'으로 설정
     });
-    inputMessage.value = '';
   }
 }
 
@@ -217,84 +283,80 @@ function sendMessage(event) {
 async function getMedia() {
   try {
     // 현 기기에서 가용 가능한 카메라, 오디오 정보를 불러옴
+    // deviceId, label
     const devices = await navigator.mediaDevices.enumerateDevices();
-    const videos = devices.filter((device) => device.kind === 'videoinput');
-    const audios = devices.filter((device) => device.kind === 'audioinput');
-
-    // 카메라 선택창 생성
-    const videoSelections = document.querySelector('select[name="videos"]');
-    if (videos) {
-      videos.forEach((video) => {
-        const option = document.createElement('option');
-        option.value = video.deviceId;
-        option.text = video.label;
-        videoSelections.appendChild(option);
-      });
-    } else {
-      const notVideo = videoSelections.querySelector('option:disabled');
-      notVideo.innerText = '사용 가능한 비디오가 없습니다.'
-    }
-
-    // 오디오 선택창 생성
-    const audioSelections = document.querySelector('select[name="audios"]');
-    if(audios){
-      audios.forEach((audio) => {
-        const option = document.createElement('option');
-        option.value = audio.deviceId;
-        option.text = audio.label;
-        audioSelections.appendChild(option);
-      });
-    } else {
-      const notAudio = audioSelections.querySelector('option:disabled');
-      notAudio.innerText = '사용 가능한 마이크가 없습니다.'
-    }
+    communication.micList.value = devices.filter((device) => device.kind === 'videoinput');
+    communication.cameraList.value = devices.filter((device) => device.kind === 'audioinput');
   } catch (error) {
     console.error('미디어 정보 불러오기 실패 : ', error);
   }
 }
 
-// 비디오 상태 토글 이벤트
-function handleVideoBtn() {
-  // 세션에 참가하지 않은 경우 무시
+// 시스템이 사용자의 마이크 설정 제어
+function handleMicBySystem(micStatus) {
+  // 세션 미참가자이면 무시
   if (!openVidu.publisher.value) return;
 
-  // 비디오 상태 토글
-  communication.isVideoOn.value = !communication.isVideoOn.value;
-
-  // 비디오 상태에 따른 텍스트 변화
-  const videoActivate = document.getElementById('video-activate')
-  if (communication.isVideoOn.value) {   
-    videoActivate.innerText = '비디오 ON'
+  // true면 마이크 제어권 부여
+  // false면 마이크 제어권 박탈, 마이크 강제 OFF
+  if (micStatus) {
+    communication.isMicHandleAvailable = true;
   } else {
-    videoActivate.innerText = '비디오 OFF'
+    communication.isMicHandleAvailable = false;
+    openVidu.publisher.value.publishVideo = false;
   }
+}
 
-  // 비디오 송출 상태 적용
-  openVidu.publisher.value.publishVideo(communication.isVideoOn.value);
+// 시스템이 사용자의 카메라 설정 제어
+function handleCameraBySystem(cameraStatus) {
+  // 세션 미참가자이면 무시
+  if (!openVidu.publisher.value) return;
+
+  // true면 마이크 제어권 부여
+  // false면 마이크 제어권 박탈, 마이크 강제 OFF
+  if (cameraStatus) {
+    communication.isCameraHandleAvailable = true;
+  } else {
+    communication.isCameraHandleAvailable = false;
+    openVidu.publisher.value.publishAudio = false;
+  }
+}
+
+// 사용자가 카메라 제어
+function handleCameraByUser() {
+  // 세션에 참가하지 않았으면 무시
+  if (!openVidu.publisher.value) return;
+
+  // 비디오 상태 변경
+  communication.isCameraOn.value = !communication.isCameraOn.value;
+  openVidu.publisher.value.publishVideo(communication.isCameraOn.value);
+
+  console.log('handleCameraByUser')
 }
 
 // 마이크 상태 토클 이벤트
-function handleAudioBtn() {
+function handleMicByUser() {
   // 세션에 참가하지 않은 경우 무시
   if (!openVidu.publisher.value) return;
 
-  // 음소거 상태 토글
-  communication.isAudioOn.value = !communication.isAudioOn.value;
-  const audioActivate = document.getElementById('audio-activate')
-  if (communication.isAudioOn.value) {
-    audioActivate.innerText = '마이크 ON'
-  } else {
-    audioActivate.innerText = '마이크 OFF'
-  }
-
-  // 음소거 설정을 적용
+  // 마이크 상태 변경
+  communication.isMicOn.value = !communication.isMicOn.value;
   openVidu.publisher.value.publishAudio(communication.isAudioOn.value);
+
+  console.log(`handleMicByUser : ${communication.isMicOn.value}`)
+}
+
+function handleShareByUser() {
+  // 세션에 참가하지 않은 경우 무시
+  if (!openVidu.publisher.value) return;
+
+  console.log("handleShareByUser")
 }
   
 // 비디오 리스트에서 사용할 기기 선택 시 이벤트
 async function handleVideoChange(event) {
-  communication.selectedVideo.value = event.target.value;
-  await replaceVideoTrack(communication.selectedVideo.value);
+  communication.selectedCamera.value = event.target.value;
+  await replaceVideoTrack(communication.selectedCamera.value);
 }
 
 // 마이크 리스트에서 사용할 기기 선택 시 이벤트
@@ -352,16 +414,16 @@ async function replaceAudioTrack(deviceId) {
   </header>
   
   <!-- session이 false일때! 즉, 방에 들어가지 않았을때 -->
-  <div id="join" v-if="!session">
+  <div id="join" v-if="!openVidu.session.value">
     <div id="join-dialog">
       <div>
         <p>
-          <label>토큰</label>
-          <input v-model="token" type="text" required />
+          <label>세션 ID</label>
+          <input v-model="openVidu.sessionId.value" type="text" required />
         </p>
         <p>
           <label>유저 ID</label>
-          <input v-model="userId" type="text" required />
+          <input v-model="userInfo.userId.value" type="text" required />
         </p>
         <p>
           <button @click="joinSession">Join!</button>
@@ -370,22 +432,8 @@ async function replaceAudioTrack(deviceId) {
     </div>
   </div>
 
-  <div id="session" v-if="session">
+  <div id="session" v-if="openVidu.session.value">
     <div id="session-header">
-      <!-- 캠활성화, 음소거 버튼 -->
-      <button id="video-activate" @click="handleVideoBtn">캠 비활성화</button>
-      <button id="audio-activate" @click="handleAudioBtn">음소거 활성화</button>
-      
-      <!-- 캠,오디오 선택 옵션 -->
-      <div>
-        <select name="videos" @change="handleVideoChange">
-          <option disabled>사용할 카메라를 선택하세요</option>
-        </select>
-        <select name="audios" @change="handleAudioChange">
-          <option disabled>사용할 마이크를 선택하세요</option>
-        </select>
-      </div>
-      
       <!-- 세션 나가기 여부 -->
       <input
         type="button"
@@ -394,45 +442,28 @@ async function replaceAudioTrack(deviceId) {
         value="Leave session"
       />
     </div>
-    
-    <!-- A팀 캠 -->
-    <div id="teamA-container">
-      <h3>A팀 캠</h3>
-      <UserVideo
-        v-for="sub in subscribersLeftComputed"
-        :key="sub.stream.connection.connectionId"
-        :stream-manager="sub"
-      />
-    </div>
-
-    <!-- B팀 캠 -->
-    <div id="teamA-container">
-      <h3>B팀 캠</h3>
-      <UserVideo
-        v-for="sub in subscribersRightComputed"
-        :key="sub.stream.connection.connectionId"
-        :stream-manager="sub"
-      />
-    </div>
+    <DebateVideos 
+      :subscribers-left="openVidu.subscribersLeftComputed"
+      :subscribers-right="openVidu.subscribersRightComputed" />
 
     <!-- 방에 들어갔을 때 같이 보이게 될 채팅창 -->
-    <div id="chat-container">
-      <div id="chat-window">
-        <ul id="chat-history">
-          <li v-for="(message, index) in messages" :key="index">
-            <strong>{{message.username}}:</strong> {{message.message}}
-          </li>
-        </ul>
-      </div>
-      <form id="chat-write">
-        <input type="text" placeholder="전달할 내용을 입력하세요." v-model="inputMessage">
-        <button @click="sendMessage">전송</button>
-      </form>
-    </div>
+    <ChattingBar 
+      :nickname="userInfo.nickname.value" :role="userInfo.team.value"
+      :messages-left="chatting.messagesLeft.value" :messages-right="chatting.messagesRight.value" 
+      :messages-all="chatting.messagesAll.value"
+       />
   </div>
 
   <footer>
-    <DebateBottomBar />
+    <DebateBottomBar
+      :team="userInfo.team.value"
+      :is-mic-handle-available="communication.isMicHandleAvailable.value"
+      :is-camera-handle-available="communication.isCameraHandleAvailable.value"
+      :is-share-handle-available="communication.isShareHandleAvailable.value"
+      :is-mic-on="communication.isMicOn.value" :is-camera-on="communication.isCameraOn.value" :is-share-on="communication.isShareOn.value" 
+      :handle-mic-by-user="handleMicByUser"
+      @handleCameraByUser="handleCameraByUser" @handleMicByUser="handleMicByUser" @handleShareByUser="handleShareByUser"
+      />
   </footer>
 </template>
 
