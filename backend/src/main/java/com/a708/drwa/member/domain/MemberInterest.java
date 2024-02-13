febb2d35485d8e4561393f485b2a0fa3d0f943b3
lkt9899 <lkt9899@naver.com>
2024-02-13 10:24:15 +0900

    Feat : 양방향 매핑 및 편의 메서드 구현

--- backend/src/main/java/com/a708/drwa/member/domain/MemberInterest.java
+++ backend/src/main/java/com/a708/drwa/member/domain/MemberInterest.java
@@ -2,6 +2,7 @@ package com.a708.drwa.member.domain;
 
 import com.a708.drwa.debate.enums.DebateCategory;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,22 +10,30 @@ import lombok.Setter;
 @Entity
 @Getter
 @NoArgsConstructor
-@Table(name = "member_interest")
 public class MemberInterest {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long memberInterestId;
+    private Integer memberInterestId;
 
     // 지연로딩 : 관심사를 조회할 때 회원 정보는 조회하지 않는다.
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
-    @Setter
     private Member member;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "debate_category")
-    @Setter
     private DebateCategory debateCategory;
 
+    @Builder
+    public MemberInterest(Member member, DebateCategory debateCategory) {
+        this.member = member;
+        this.debateCategory = debateCategory;
+        member.getMemberInterestList().add(this);
+    }
+
+    public void removeAssociations() {
+        this.member.removeInterest(this);
+        this.member = null;
+    }
 }
