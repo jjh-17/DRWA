5c0a6024e967c9a9796598c30541904ce8de49e8
lkt9899 <lkt9899@naver.com>
2024-02-13 09:33:17 +0900

    Feat : S3 서버 연동

--- backend/src/main/java/com/a708/drwa/DrwaApplication.java
+++ backend/src/main/java/com/a708/drwa/DrwaApplication.java
@@ -6,11 +6,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @EnableScheduling
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-@EnableCaching
 public class DrwaApplication {
-
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DrwaApplication.class, args);
 	}
