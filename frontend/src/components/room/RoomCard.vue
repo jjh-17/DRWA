d0c05ba294fddb29e44c24b2f879c7c3fd0fdf03
KimJihwan98 <wlghks98k@gmail.com>
2024-02-14 09:49:47 +0900

    Refactor : 썸네일 크기 수정

--- frontend/src/components/room/RoomCard.vue
+++ frontend/src/components/room/RoomCard.vue
@@ -1,70 +1,89 @@
 <template>
-  <div class="room-card">
-      <div class="top-container"></div>
-      <div class="thumbnail-part">
-        <div class="image-container">
-          <img :src="room.thumbnail1" alt="Image 1" />
-        </div>
-        <!-- <div>vs</div> -->
-        <div class="image-container">
-          <img :src="room.thumbnail2" alt="Image 2" />
-        </div>
+  <div class="room-card" @click="joinDebate(room.sessionId)">
+    <div class="top-container"></div>
+    <div class="thumbnail-part">
+      <div class="image-container">
+        <img :src="room.thumbnail1" alt="Image 1" />
       </div>
-      <div class="title">제목 : {{ room.title }}</div>
-      <div class="keyword">제시어 : {{ room.leftKeyword }} vs {{ room.rightKeyword }}</div>
-      <div class="host">방장 : {{ room.hostId }}</div>
+      <!-- <div>vs</div> -->
+      <div class="image-container">
+        <img :src="room.thumbnail2" alt="Image 2" />
+      </div>
     </div>
-  </template>
-  
-  <script setup>
-  import { defineProps } from 'vue'
+    <div class="title">제목 : {{ room.title }}</div>
+    <div class="keyword">제시어 : {{ room.leftKeyword }} vs {{ room.rightKeyword }}</div>
+    <div class="host">방장 : {{ room.hostName }}</div>
+  </div>
+</template>
 
-  const props = defineProps({
-    room: Object,
-  })
-  
-  </script>
-  
-  <style scoped>
-  .top-container {
-    height:10px;
-    background-color:#34227C;
+<script setup>
+import { defineProps } from 'vue'
+import { useRouter } from 'vue-router';
+import { httpService } from '@/api/axios'
+
+const router = useRouter();
+
+
+const joinDebate = async (sessionId) => {
+  try {
+    const response = await httpService.get(`/openvidu/session/${sessionId}`);
+    console.log('연결 정보 응답:', response.data);
+    router.push(`/debate/${sessionId}`);
+  } catch (error) {
+    console.error('연결 정보 가져오기 에러:', error);
   }
-  .room-card {
-    margin: 30px;
-    display: flex;
-    flex-direction: column; /* 카드 내용을 세로로 배치 */
-    align-items: left;
-    /* padding: 16px; */
-    border: 1px solid #ccc;
-    border-radius: 8px;
-    background-color: #34227C;
-  }
-  
-  .thumbnail-part {
-    background-color:#f9f9f9;
-    margin-bottom:10px;
-    padding:10px;
-    display: flex; /* 이미지를 가로로 배치 */
-    justify-content: center; /* 이미지를 중앙 정렬 */
-    width: 100%; /* 썸네일 부분의 너비를 room-card의 너비에 맞춤 */
-  }
-  .title,.keyword,.host {
-    color:#f9f9f9;
-    text-align:left;
-    font-weight: bold;
-    padding-left:16px;
-    padding-bottom:5px;
-  }
-  .image-container img {
-    max-width: 100%;
-    height: auto;
-    display: block; /* 이미지를 블록 요소로 만들어 컨테이너의 크기에 맞춤 */
-  }
-  
-  .image-container {
-    flex: 1; /* 이미지 컨테이너들이 공간을 균등하게 차지하도록 함 */
-    padding: 0 0px; /* 이미지 사이에 간격 추가 */
-  }
-  </style>
-  
+}
+
+
+const props = defineProps({
+  room: Object,
+})
+</script>
+
+<style scoped>
+.top-container {
+  height: 10px;
+  background-color: #34227c;
+}
+.room-card {
+  margin: 30px;
+  display: flex;
+  flex-direction: column; /* 카드 내용을 세로로 배치 */
+  align-items: left;
+  /* padding: 16px; */
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  background-color: #34227c;
+}
+
+.thumbnail-part {
+  background-color: #f9f9f9;
+  margin-bottom: 10px;
+  padding: 10px;
+  display: flex; /* 이미지를 가로로 배치 */
+  justify-content: center; /* 이미지를 중앙 정렬 */
+  width: 100%; /* 썸네일 부분의 너비를 room-card의 너비에 맞춤 */
+}
+.title,
+.keyword,
+.host {
+  color: #f9f9f9;
+  text-align: left;
+  font-weight: bold;
+  padding-left: 16px;
+  padding-bottom: 5px;
+}
+.image-container img {
+  max-width: 100%;
+  /* height: auto; */
+  width:150px;
+  height:100px;
+  display: block; /* 이미지를 블록 요소로 만들어 컨테이너의 크기에 맞춤 */
+}
+
+.image-container {
+  flex: 1; 
+  max-width:100%;
+}
+
+</style>
