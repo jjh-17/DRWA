1f2e2cdc3b7a96dea7db45c80dcd9a07766610d2
minwoo-kwak <minwoo.kwak540@gmail.com>
2024-02-15 18:13:20 +0900

    Fix : 로그인시 랭킹 못 가져오는 오류 레디스에 해당 회원의 랭킹 정보들이 사라져서 Member Not Found 가 떴다고 판단했습니다.
    그래서 멤버의 랭킹 정보를 가져오는 부분을 주석 처리하였고, 포인트는 1000점으로 고정시켜놓았습니다.

--- backend/src/main/java/com/a708/drwa/member/service/MemberService.java
+++ backend/src/main/java/com/a708/drwa/member/service/MemberService.java
@@ -114,10 +114,11 @@ public class MemberService {
         List<MemberInterest> memberInterests = memberRepository.findById(jwtMemberInfo.getMemberId())
                 .orElseThrow(() -> new MemberException(MemberErrorCode.MEMBER_NOT_FOUND))
                 .getMemberInterestList();
+
         // 프로필 조회
         ProfileResponse profile = profileService.findProfileByMemberId(jwtMemberInfo.getMemberId());
 
-        // 프로필 이미지 URL 조회
+        // 프로필 이미지 URL 조회ㄱ
         String profileImageUrl = profileImageService.findProfileImageUrlByMemberId(jwtMemberInfo.getMemberId());
 
         // 응답 DTO 반환
