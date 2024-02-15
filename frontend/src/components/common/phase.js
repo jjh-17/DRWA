8ed521b510a14e0b3d3cc4c824665330b83406b7
KimJihwan98 <wlghks98k@gmail.com>
2024-02-15 20:26:50 +0900

    Feat : phase common 파일 추가

---
+++ frontend/src/components/common/phase.js
@@ -0,0 +1,17 @@
+export const phase = [
+    { phaseNum: 0, korean: '없어도됨' },
+    { phaseNum: 1, korean: 'teamA 발언시간' },
+    { phaseNum: 2, korean: '질문 시간' },
+    { phaseNum: 3, korean: 'teamB 발언시간' },
+    { phaseNum: 4, korean: '질문 시간' },
+    { phaseNum: 5, korean: 'teamA 발언시간' },
+    { phaseNum: 6, korean: '질문 시간' },
+    { phaseNum: 7, korean: 'teamB 발언시간' },
+    { phaseNum: 8, korean: '질문 시간' },
+    { phaseNum: 9, korean: 'teamA 발언시간' },
+    { phaseNum: 10, korean: '질문 시간' },
+    { phaseNum: 11, korean: 'teamB 발언시간' },
+    { phaseNum: 12, korean: '질문 시간' },
+    { phaseNum: 20, korean: '토론 끝'},
+  ]
+  
