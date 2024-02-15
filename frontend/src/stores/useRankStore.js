d17e8a33b4bcc2e034b5bf06d25b686720e131fb
noowadc <cha12608@naver.com>
2024-02-16 00:40:19 +0900

    Refactor : API 호출을 httpService 사용

--- frontend/src/stores/useRankStore.js
+++ frontend/src/stores/useRankStore.js
@@ -1,7 +1,6 @@
 import { defineStore } from 'pinia';
-import axios from 'axios';
+import { httpService } from '@/api/axios'; 
 
-const REST_ROOM_API = `http://i10a708.p.ssafy.io/ranking`
 export const useRankStore = defineStore('rankStore', {
   state: () => ({
     rankings: [],
@@ -9,7 +8,7 @@ export const useRankStore = defineStore('rankStore', {
   actions: {
     async fetchRankings(category) {
       try {
-        const response = await axios.get(`${REST_ROOM_API}/${category}`);
+        const response = await httpService.get(`ranking/${category}`);
         this.rankings = response.data;
       } catch (error) {
         console.error("Error fetching rankings:", error);
@@ -17,7 +16,7 @@ export const useRankStore = defineStore('rankStore', {
     },
     async searchUserRank(nickname) {
       try {
-        const response = await axios.get(`${REST_ROOM_API}/${nickname}`);
+        const response = await httpService.get(`ranking/${nickname}`);
         this.rankings = [response.data];
       } catch (error) {
         console.error("Error searching user rank:", error);
